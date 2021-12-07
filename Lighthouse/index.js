5f0ac5d860362d868268dc880f4e3325a7841da5
ravish1729 <ravish1729@gmail.com>
2021-12-08 00:20:57 +0530

    prototype version 0.1 lint

--- Lighthouse/index.js
+++ Lighthouse/index.js
@@ -1,138 +1,157 @@
-const axios = require('axios');
+const axios = require("axios");
 const FormData = require("form-data");
-const fs = require('fs');
-const mime = require('mime-types')
-const IPFS = require('ipfs')
-const EthCrypto = require('eth-crypto');
-const CryptoJS = require('crypto-js');
+const fs = require("fs");
+const mime = require("mime-types");
+const IPFS = require("ipfs");
+const EthCrypto = require("eth-crypto");
+const CryptoJS = require("crypto-js");
 
-const URL = 'http://localhost:8000';
+const URL = "http://localhost:8000";
 
-exports.create_wallet = async (password) =>{
-  try{
-    const response = await axios.post(URL+'/api/wallet/create_wallet', {"password": password});
+exports.create_wallet = async (password) => {
+  try {
+    const response = await axios.post(URL + "/api/wallet/create_wallet", {
+      password: password,
+    });
     return response.data;
-  } catch{
+  } catch {
     return null;
   }
-}
+};
 
-exports.get_balance = async (publicKey) =>{
-  try{
-    const response = await axios.post(URL+'/api/wallet/get_balance', {"publicKey": publicKey});
+exports.get_balance = async (publicKey) => {
+  try {
+    const response = await axios.post(URL + "/api/wallet/get_balance", {
+      publicKey: publicKey,
+    });
     return response.data;
-  } catch{
+  } catch {
     return null;
   }
-}
+};
 
-exports.user_token = async (expiry_time) =>{
-  try{
-    const response = await axios.get(URL+'/api/estuary/user_token?expiry_time='+ expiry_time);
+exports.user_token = async (expiry_time) => {
+  try {
+    const response = await axios.get(
+      URL + "/api/estuary/user_token?expiry_time=" + expiry_time
+    );
     return response.data;
-  } catch{
+  } catch {
     return null;
   }
-}
+};
 
-exports.deploy = async (path, token) =>{
+exports.deploy = async (path, token) => {
   var formData = new FormData();
   formData.append("data", fs.createReadStream(path));
 
   const headers = formData.getHeaders();
-  const response = await axios.post('https://shuttle-1.estuary.tech/content/add', formData, { 
+  const response = await axios.post(
+    "https://shuttle-1.estuary.tech/content/add",
+    formData,
+    {
       headers: {
-          Authorization: `Bearer ${token}`,
-          ...headers,
-      }}
+        Authorization: `Bearer ${token}`,
+        ...headers,
+      },
+    }
   );
   return response.data;
-}
+};
 
 function bytesToSize(bytes) {
-  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-  if (bytes === 0) return '0 Byte';
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
+  if (bytes === 0) return "0 Byte";
   const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10);
   return `${Math.round(bytes / Math.pow(1024, i), 2)} ${sizes[i]}`;
 }
 
-exports.get_quote = async (path, publicKey) =>{
-  try{
+exports.get_quote = async (path, publicKey) => {
+  try {
     const stats = fs.statSync(path);
     const mime_type = mime.lookup(path);
     const fileSizeInBytes = stats.size;
-    const file_name = path.split('/').pop();
+    const file_name = path.split("/").pop();
 
     // disable logs of IPFS.create()
-    const console_log = console.log; 
-    console.log = function() {};
+    const console_log = console.log;
+    console.log = function () {};
     const node = await IPFS.create();
-	  const file = await node.add(path, {onlyHash: true, cidVersion: 1});
+    const file = await node.add(path, { onlyHash: true, cidVersion: 1 });
     const ipfs_hash = file.path;
     node.stop();
     console.log = console_log;
 
     const body = {
-      "fileSize": fileSizeInBytes,
-      "publicKey": publicKey,
-      "ipfs_hash": ipfs_hash,
-    }
+      fileSize: fileSizeInBytes,
+      publicKey: publicKey,
+      ipfs_hash: ipfs_hash,
+    };
     const response = await axios.post(URL + `/api/estuary/get_quote`, body);
-    
+
     response.data.file_size = bytesToSize(fileSizeInBytes);
     response.data.mime_type = mime_type;
     response.data.file_name = file_name;
     response.data.fee = response.data.cost + response.data.gasFee;
     response.data.ipfs_hash = ipfs_hash;
-    
+
     return response.data;
-  } catch(err){
+  } catch (err) {
     return null;
   }
-}
+};
 
-exports.push_cid_tochain = async(privateKey, cid) => {
-  try{
+exports.push_cid_tochain = async (privateKey, cid) => {
+  try {
     const body = {
-      "privateKey": privateKey,
-      "cid": cid,
-    }
-    const response = await axios.post(URL + `/api/estuary/push_cid_tochain`, body);
+      privateKey: privateKey,
+      cid: cid,
+    };
+    const response = await axios.post(
+      URL + `/api/estuary/push_cid_tochain`,
+      body
+    );
     return response.data;
-  } catch{
+  } catch {
     return null;
   }
-}
+};
 
-exports.status = async (cid) =>{
-  const response = await axios.get(URL+`/api/estuary/status/${cid}`);
+exports.status = async (cid) => {
+  const response = await axios.get(URL + `/api/estuary/status/${cid}`);
   return response.data;
-}
+};
 
-exports.list_data = async (offset, limit) =>{
-  if(offset!==undefined && limit!==undefined) {
-    const response = await axios.get(URL+`/api/estuary/list_data?offset=${offset}&limit=${limit}`);
+exports.list_data = async (offset, limit) => {
+  if (offset !== undefined && limit !== undefined) {
+    const response = await axios.get(
+      URL + `/api/estuary/list_data?offset=${offset}&limit=${limit}`
+    );
     return response.data;
-  } else{
-      const response = await axios.get(URL+'/api/estuary/list_data?offset=0&limit=2');
-      return response.data;
+  } else {
+    const response = await axios.get(
+      URL + "/api/estuary/list_data?offset=0&limit=2"
+    );
+    return response.data;
   }
-}
+};
 
-exports.get_deals = async (content_id) =>{
-  const response = await axios.get(URL+`/api/estuary/get_deals?content_id=${content_id}`);
+exports.get_deals = async (content_id) => {
+  const response = await axios.get(
+    URL + `/api/estuary/get_deals?content_id=${content_id}`
+  );
   return response.data;
-}
+};
 
-exports.get_key = async(encPrivateKey, password) => {
-  try{
-    const bytes  = CryptoJS.AES.decrypt(encPrivateKey, password);
+exports.get_key = async (encPrivateKey, password) => {
+  try {
+    const bytes = CryptoJS.AES.decrypt(encPrivateKey, password);
     const originalText = bytes.toString(CryptoJS.enc.Utf8);
     const publicKey = EthCrypto.publicKeyByPrivateKey(originalText);
     const address = EthCrypto.publicKey.toAddress(publicKey);
 
-    return({privateKey: originalText, publicKey: address})
-  } catch{
+    return { privateKey: originalText, publicKey: address };
+  } catch {
     return null;
   }
-}
+};
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const yargs = require('yargs');
-const chalk = require('chalk');
-const util = require('util');
+const yargs = require("yargs");
+const chalk = require("chalk");
+const util = require("util");
 const read = require("read");
-const fs = require('fs');
-const Conf = require('conf');
+const fs = require("fs");
+const Conf = require("conf");
 
-const Lighthouse = require('./Lighthouse');
+const Lighthouse = require("./Lighthouse");
 
 /*
 colour scheme for cli
@@ -15,247 +15,299 @@ colour scheme for cli
     warning - yellow
 */
 const config = new Conf();
-yargs.version('0.1.0');
-yargs.usage('Usage: lighthouse-web3' + chalk.cyan(' [command] ') + chalk.green('[options]'));
-yargs.alias('h', 'help');
-yargs.alias('v', 'version');
+yargs.version("0.1.0");
+yargs.usage(
+  "Usage: lighthouse-web3" +
+    chalk.cyan(" [command] ") +
+    chalk.green("[options]")
+);
+yargs.alias("h", "help");
+yargs.alias("v", "version");
 
 yargs.command({
-    command: 'create-wallet',
-    describe: 'Creates a new wallet',
-    builder: {
-        save: {
-            describe: 'Saves the wallet after creation',
-        },
+  command: "create-wallet",
+  describe: "Creates a new wallet",
+  builder: {
+    save: {
+      describe: "Saves the wallet after creation",
     },
-    handler: async function (argv) {
-        const options = {
-            prompt: 'Set a password for your wallet:',
-            silent: true,
-            default: '',
-        }
-        
-        let save_wallet = false;
-        if(argv.save) {
-            save_wallet = true;
-        }
+  },
+  handler: async function (argv) {
+    const options = {
+      prompt: "Set a password for your wallet:",
+      silent: true,
+      default: "",
+    };
 
-        read(options, async (err, result)=>{
-            const wallet = await Lighthouse.create_wallet(result.trim());
-            if(wallet){
-                fs.writeFile('wallet.json', JSON.stringify(wallet, null, 4), function(err) {
-                    if(err) {
-                        console.log(chalk.red('Creating Wallet Failed!'));
-                    }
-
-                    if(save_wallet) {
-                        config.set('Lighthouse_privateKeyEncrypted', wallet['privateKeyEncrypted']);
-                        config.set('Lighthouse_publicKey', wallet['publicKey']);
-                    }
-
-                    console.log(chalk.cyan('Public Key: ' + wallet.publicKey));                    
-                    console.log(chalk.green('Wallet Created!'));
-                });
-            }else{
-                console.log(chalk.red('Creating Wallet Failed!'));
-            }
-        })
+    let save_wallet = false;
+    if (argv.save) {
+      save_wallet = true;
     }
-})
+
+    read(options, async (err, result) => {
+      const wallet = await Lighthouse.create_wallet(result.trim());
+      if (wallet) {
+        fs.writeFile(
+          "wallet.json",
+          JSON.stringify(wallet, null, 4),
+          function (err) {
+            if (err) {
+              console.log(chalk.red("Creating Wallet Failed!"));
+            }
+
+            if (save_wallet) {
+              config.set(
+                "Lighthouse_privateKeyEncrypted",
+                wallet["privateKeyEncrypted"]
+              );
+              config.set("Lighthouse_publicKey", wallet["publicKey"]);
+            }
+
+            console.log(chalk.cyan("Public Key: " + wallet.publicKey));
+            console.log(chalk.green("Wallet Created!"));
+          }
+        );
+      } else {
+        console.log(chalk.red("Creating Wallet Failed!"));
+      }
+    });
+  },
+});
 
 yargs.command({
-    command: 'import-wallet',
-    describe: 'Import an existing wallet',
-    builder: {
-        path: {
-            describe: 'Path to wallet',
-            demandOption: true,
-            type: 'string'
-        },
+  command: "import-wallet",
+  describe: "Import an existing wallet",
+  builder: {
+    path: {
+      describe: "Path to wallet",
+      demandOption: true,
+      type: "string",
     },
-    handler: async function (argv) {
-        const wallet = JSON.parse(fs.readFileSync(argv.path, 'utf8'));
-        config.set('Lighthouse_privateKeyEncrypted', wallet['privateKeyEncrypted']);
-        config.set('Lighthouse_publicKey', wallet['publicKey']);
-        console.log(chalk.green('Wallet Imported!'));
-    }
-})
+  },
+  handler: async function (argv) {
+    const wallet = JSON.parse(fs.readFileSync(argv.path, "utf8"));
+    config.set("Lighthouse_privateKeyEncrypted", wallet["privateKeyEncrypted"]);
+    config.set("Lighthouse_publicKey", wallet["publicKey"]);
+    console.log(chalk.green("Wallet Imported!"));
+  },
+});
 
 yargs.command({
-    command: 'wallet-forget',
-    describe: 'Remove previously saved wallet',
-    handler: async function (argv) {
-        config.delete('Lighthouse_privateKeyEncrypted');
-        config.delete('Lighthouse_publicKey');
-        console.log(chalk.green('Wallet Removed!'));
-    }
-})
+  command: "wallet-forget",
+  describe: "Remove previously saved wallet",
+  handler: async function (argv) {
+    config.delete("Lighthouse_privateKeyEncrypted");
+    config.delete("Lighthouse_publicKey");
+    console.log(chalk.green("Wallet Removed!"));
+  },
+});
 
 yargs.command({
-    command: 'balance',
-    describe: 'Get current balance of your wallet',
-    handler: async function (argv) {
-        const options = {
-            prompt: 'Enter your password ',
-            silent: true,
-            default: '',
-        }
-        
-        read(options, async (err, result)=>{
-            const balance = await Lighthouse.get_balance(config.get('Lighthouse_publicKey'));
-            if(balance){
-                console.log(chalk.green('balance ' + balance.data));
-            }else{
-                console.log(chalk.red('Something Went Wrong!'));
-            }
-        })
-    }
-})
+  command: "balance",
+  describe: "Get current balance of your wallet",
+  handler: async function (argv) {
+    const options = {
+      prompt: "Enter your password ",
+      silent: true,
+      default: "",
+    };
+
+    read(options, async (err, result) => {
+      const balance = await Lighthouse.get_balance(
+        config.get("Lighthouse_publicKey")
+      );
+      if (balance) {
+        console.log(chalk.green("balance " + balance.data));
+      } else {
+        console.log(chalk.red("Something Went Wrong!"));
+      }
+    });
+  },
+});
 
 yargs.command({
-    command: 'deploy',
-    describe: 'Deploy a directory or file',
-    builder: {
-        path: {
-            describe: 'Path of file to be uploaded',
-            demandOption: true,
-            type: 'string'
-        },
+  command: "deploy",
+  describe: "Deploy a directory or file",
+  builder: {
+    path: {
+      describe: "Path of file to be uploaded",
+      demandOption: true,
+      type: "string",
     },
-    handler: async function (argv) {
-        const path = argv.path;
-        const response = await Lighthouse.get_quote(argv.path, config.get('Lighthouse_publicKey'));
-        if(response){
-            console.table([{ID: response.ipfs_hash, Size: response.file_size, Fee: response.fee, Type: response.mime_type, Name: response.file_name}], ['ID', 'Size', 'Fee', 'Type', 'Name']);
-        
-            console.log();
-            
-            console.log(chalk.cyan('Summary'));
-            console.log('Total Size: ' + response.file_size);
-            console.log('Fees: ' + response.cost + response.gasFee);
-            console.log('Total Fee: ' + response.fee);
-            
-            console.log();
-            
-            console.log(chalk.cyan('Wallet'));
-            console.log('Address: ' + config.get('Lighthouse_publicKey'));
-            console.log('Current balance: ' + response.current_balance);
-            const balance_after_deploy = (Number(response.current_balance) - response.fee)*(10**(-18))
-            console.log('Balance after deploy: ' + balance_after_deploy);
-            
-            console.log();
-            
-            console.log(chalk.green('Carefully check the above details are correct, then confirm to complete this upload') + 'Y/n');
+  },
+  handler: async function (argv) {
+    const path = argv.path;
+    const response = await Lighthouse.get_quote(
+      argv.path,
+      config.get("Lighthouse_publicKey")
+    );
+    if (response) {
+      console.table(
+        [
+          {
+            ID: response.ipfs_hash,
+            Size: response.file_size,
+            Fee: response.fee,
+            Type: response.mime_type,
+            Name: response.file_name,
+          },
+        ],
+        ["ID", "Size", "Fee", "Type", "Name"]
+      );
 
-            const options = {
-                prompt: '',
-                default: 'n',
-            }
-            
-            
-            read(options, async (err, result)=>{
-                if(result.trim() == 'Y' || result.trim() == 'y' || result.trim() == 'yes'){
+      console.log();
 
-                    if(balance_after_deploy<0){
-                        console.log(chalk.red('Insufficient balance!'));
-                        process.exit();
-                    } else {
-                        const options = {
-                            prompt: 'Enter your password: ',
-                            silent: true,
-                            default: '',
-                        }
-                        
-                        read(options, async (err, password)=>{
+      console.log(chalk.cyan("Summary"));
+      console.log("Total Size: " + response.file_size);
+      console.log("Fees: " + response.cost + response.gasFee);
+      console.log("Total Fee: " + response.fee);
 
-                            const key = await Lighthouse.get_key(config.get('Lighthouse_privateKeyEncrypted'), password.trim());
-                            if(key){
-                                // Upload File
-                                const upload_token = await Lighthouse.user_token('24h');
-                                const deploy = await Lighthouse.deploy(path, upload_token.token);
-                                console.log(chalk.green('File Deployed, visit following url to view content!'));
-                                console.log(chalk.cyan('Visit: ' +'https://dweb.link/ipfs/'+ deploy.cid));
-                                console.log('CID: ' + deploy.cid);
-                                
-                                console.log();
-    
-                                // Push CID to chain
-                                console.log(chalk.green('Pushing CID to chain'));
-                                const transaction = await Lighthouse.push_cid_tochain(key.privateKey, deploy.cid);
-                                console.log('Transaction: '+ 'https://polygonscan.com/tx/' + transaction.hash);
-                                console.log(chalk.green('CID pushed to chain'));
+      console.log();
 
-                                process.exit();
-                            }else{
-                                console.log(chalk.red('Something Went Wrong!'));
-                                process.exit();
-                            }
-                        })
-                    }
-                } else{
-                    console.log(chalk.red('Upload Cancelled!'));
-                    process.exit();
-                }
-            })
-        } else{
-            console.log(chalk.red('Something Went Wrong!'));
+      console.log(chalk.cyan("Wallet"));
+      console.log("Address: " + config.get("Lighthouse_publicKey"));
+      console.log("Current balance: " + response.current_balance);
+      const balance_after_deploy =
+        (Number(response.current_balance) - response.fee) * 10 ** -18;
+      console.log("Balance after deploy: " + balance_after_deploy);
+
+      console.log();
+
+      console.log(
+        chalk.green(
+          "Carefully check the above details are correct, then confirm to complete this upload"
+        ) + "Y/n"
+      );
+
+      const options = {
+        prompt: "",
+        default: "n",
+      };
+
+      read(options, async (err, result) => {
+        if (
+          result.trim() == "Y" ||
+          result.trim() == "y" ||
+          result.trim() == "yes"
+        ) {
+          if (balance_after_deploy < 0) {
+            console.log(chalk.red("Insufficient balance!"));
             process.exit();
+          } else {
+            const options = {
+              prompt: "Enter your password: ",
+              silent: true,
+              default: "",
+            };
+
+            read(options, async (err, password) => {
+              const key = await Lighthouse.get_key(
+                config.get("Lighthouse_privateKeyEncrypted"),
+                password.trim()
+              );
+              if (key) {
+                // Upload File
+                const upload_token = await Lighthouse.user_token("24h");
+                const deploy = await Lighthouse.deploy(
+                  path,
+                  upload_token.token
+                );
+                console.log(
+                  chalk.green(
+                    "File Deployed, visit following url to view content!"
+                  )
+                );
+                console.log(
+                  chalk.cyan("Visit: " + "https://dweb.link/ipfs/" + deploy.cid)
+                );
+                console.log("CID: " + deploy.cid);
+
+                console.log();
+
+                // Push CID to chain
+                console.log(chalk.green("Pushing CID to chain"));
+                const transaction = await Lighthouse.push_cid_tochain(
+                  key.privateKey,
+                  deploy.cid
+                );
+                console.log(
+                  "Transaction: " +
+                    "https://polygonscan.com/tx/" +
+                    transaction.hash
+                );
+                console.log(chalk.green("CID pushed to chain"));
+
+                process.exit();
+              } else {
+                console.log(chalk.red("Something Went Wrong!"));
+                process.exit();
+              }
+            });
+          }
+        } else {
+          console.log(chalk.red("Upload Cancelled!"));
+          process.exit();
         }
+      });
+    } else {
+      console.log(chalk.red("Something Went Wrong!"));
+      process.exit();
     }
-})
+  },
+});
 
 yargs.command({
-    command: 'status',
-    describe: 'Get metadata around the storage per CID',
-    builder: {
-        cid: {
-            describe: 'CID of the storage',
-            demandOption: true,
-            type: 'string'
-        }
+  command: "status",
+  describe: "Get metadata around the storage per CID",
+  builder: {
+    cid: {
+      describe: "CID of the storage",
+      demandOption: true,
+      type: "string",
     },
-    handler: async function (argv) {
-        const response = await Lighthouse.status(argv.cid);
-        console.log(util.inspect(response, false, null, true));
-    }
-})
+  },
+  handler: async function (argv) {
+    const response = await Lighthouse.status(argv.cid);
+    console.log(util.inspect(response, false, null, true));
+  },
+});
 
 yargs.command({
-    command: 'list-data',
-    describe: 'List all of the data you have pinned to IPFS',
-    builder: {
-        offset: {
-            describe: 'starting of list',
-            demandOption: false,
-            type: 'string'
-        },
-        limit: {
-            describe: 'number of items to return',
-            demandOption: false,
-            type: 'string'
-        }
+  command: "list-data",
+  describe: "List all of the data you have pinned to IPFS",
+  builder: {
+    offset: {
+      describe: "starting of list",
+      demandOption: false,
+      type: "string",
     },
-    handler: async function (argv) {
-        const response = await Lighthouse.list_data(argv.offset, argv.limit);
-        console.log(util.inspect(response, false, null, true));
-    }
-})
+    limit: {
+      describe: "number of items to return",
+      demandOption: false,
+      type: "string",
+    },
+  },
+  handler: async function (argv) {
+    const response = await Lighthouse.list_data(argv.offset, argv.limit);
+    console.log(util.inspect(response, false, null, true));
+  },
+});
 
 yargs.command({
-    command: 'get-deals',
-    describe: 'Get all of the deals being made for a specific Content ID stored',
-    builder: {
-        content_id: {
-            describe: 'specific content id',
-            demandOption: true,
-            type: 'string'
-        }
+  command: "get-deals",
+  describe: "Get all of the deals being made for a specific Content ID stored",
+  builder: {
+    content_id: {
+      describe: "specific content id",
+      demandOption: true,
+      type: "string",
     },
-    handler: async function (argv) {
-        const response = await Lighthouse.get_deals(argv.content_id);
-        console.log(util.inspect(response.data, false, null, true));
-    }
-})
+  },
+  handler: async function (argv) {
+    const response = await Lighthouse.get_deals(argv.content_id);
+    console.log(util.inspect(response.data, false, null, true));
+  },
+});
 
 yargs.argv;
 module.exports = Lighthouse;
