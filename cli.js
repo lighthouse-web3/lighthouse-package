6adbf06b45cac4df550e2ff0b981d8a79b84494e
ravish1729 <ravish1729@gmail.com>
2021-12-19 18:38:05 +0530

    cli and npm package working

---
+++ cli.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+const yargs = require("yargs");
+
+yargs.version("1.1.0");
+yargs.commandDir('Commands');
+yargs.parserConfiguration({
+  "parse-numbers": false,
+});
+yargs.alias("v", "version");
+yargs.help(false);
+yargs.parse().argv;
--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-#!/usr/bin/env node
-
-const yargs = require("yargs");
 
 const Lighthouse = require("./Lighthouse");
-
-yargs.version("1.1.0");
-yargs.commandDir('Commands');
-yargs.parserConfiguration({
-  "parse-numbers": false,
-});
-yargs.alias("v", "version");
-yargs.help(false);
-yargs.parse();
-
-yargs.argv;
 module.exports = Lighthouse;
