d01a91b69bc7284e920839d201cc8d8b1412fcb1
ravish1729 <ravish1729@gmail.com>
2021-12-20 13:15:28 +0530

    help console fixes

--- Commands/help.js
+++ Commands/help.js
@@ -3,51 +3,21 @@ const chalk = require("chalk");
 module.exports = {
   command: "$0",
   handler: async function (argv) {
-    console.log(chalk.yellow("Welcome to lighthouse-web3"));
-    console.log();
-    console.log(
-      "Usage: lighthouse-web3" +
-        chalk.cyan(" [command] ") +
-        chalk.green("[options]")
-    );
-    console.log();
-    console.log(
-      chalk.green("Commands (alias)") +
-        chalk.grey("                     Description")
-    );
-    console.log(
-      "create-wallet   " + "                     Creates a new wallet"
-    );
-    console.log(
-      "import-wallet   " + "                     Import an existing wallet"
-    );
-    console.log(
-      "wallet-forget   " + "                     Remove previously saved wallet"
-    );
-    console.log(
-      "balance         " +
-        "                     Get current balance of your wallet"
-    );
-    console.log(
-      "deploy          " + "                     Deploy a directory or file"
-    );
-    console.log(
-      "status          " +
-        "                     Get metadata around the storage per CID"
-    );
-    console.log();
+    console.log(chalk.yellow("Welcome to lighthouse-web3\n"));
+    console.log("Usage: lighthouse-web3" +chalk.cyan(" [command] ") +chalk.green("[options]\n"));
+    console.log(chalk.green("Commands (alias)") +chalk.grey("                     Description"));
+    console.log("create-wallet" + Array(24).fill('\xa0').join('') + "Creates a new wallet");
+    console.log("import-wallet" + Array(24).fill('\xa0').join('') + "Import an existing wallet");
+    console.log("wallet-forget" + Array(24).fill('\xa0').join('') + "Remove previously saved wallet");
+    console.log("balance" + Array(30).fill('\xa0').join('') +"Get current balance of your wallet");
+    console.log("deploy" + Array(31).fill('\xa0').join('') + "Deploy a directory or file");
+    console.log("status" + Array(31).fill('\xa0').join('') +"Get metadata around the storage per CID\n");
     console.log(chalk.cyan("Options"));
-    console.log(
-      "--save          " +
-        "                     Saves the wallet after creation"
-    );
-    console.log();
+    console.log("--save" + Array(31).fill('\xa0').join('') +"Saves the wallet after creation\n");
     console.log(chalk.magenta("Example"));
     console.log("Create wallet and save it");
-    console.log("   lighthouse-web3 create-wallet --save");
-    console.log();
+    console.log(Array(5).fill('\xa0').join('') + "lighthouse-web3 create-wallet --save\n");
     console.log("Import wallet");
-    console.log("   lighthouse-web3 import-wallet --path wallet.json");
-    console.log();
+    console.log(Array(5).fill('\xa0').join('') + "lighthouse-web3 import-wallet --path wallet.json\n");
   }
 };
