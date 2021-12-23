b82746f58c81c03bc866d22ed95d4adbef6b0b09
ravish1729 <ravish1729@gmail.com>
2021-12-23 22:36:24 +0530

    status done

--- Commands/status.js
+++ Commands/status.js
@@ -1,5 +1,6 @@
 const chalk = require("chalk");
 const Lighthouse = require("../Lighthouse");
+const { bytesToSize } = require("./byteToSize");
 
 module.exports = {
   command: "status <cid>",
@@ -15,15 +16,38 @@ module.exports = {
       const response = await Lighthouse.status(argv.cid);
       console.log();
       console.log(
-        chalk.yellow("CID        : ") + response[0]["content"]["cid"]
+        chalk.yellow("CID:") + Array(9).fill("\xa0").join("") + response[0]["content"]["cid"]
       );
       console.log(
-        chalk.yellow("Name       : ") + response[0]["content"]["name"]
+        chalk.yellow("Name:") + Array(8).fill("\xa0").join("") + response[0]["content"]["name"]
       );
       console.log(
-        chalk.yellow("Replication: ") + response[0]["content"]["replication"]
+        chalk.yellow("Size:") + Array(8).fill("\xa0").join("") + bytesToSize(response[0]["content"]["size"])
+      );
+      console.log(
+        chalk.yellow("Replication:") + Array(1).fill("\xa0").join("") + response[0]["content"]["replication"]
       );
       console.log();
+      
+      for(let i=0; i<response.length; i++){
+        if(response[i]["deals"].length>0){
+          console.log(Array(20).fill("\xa0").join("") 
+                      + chalk.yellow("Miner : ") 
+                      + Array(10).fill("\xa0").join("")
+                      + chalk.yellow("DealId: "));
+          for(let j=0; j<response[i]["deals"].length; j++){
+            let gap = 10 + (8 - response[i]["deals"][j]["miner"].length)
+            console.log(Array(20).fill("\xa0").join("") 
+                        + response[i]["deals"][j]["miner"]
+                        + Array(gap).fill("\xa0").join("")
+                        + response[i]["deals"][j]["dealId"]);
+          }
+        } else{
+          console.log("CID push to filecoin network in progress.");  
+        }
+        break
+      }
+
     }
   },
 };
