6eb100ffc76b512f3aca322389ab0657dabd5f53
ravish1729 <ravish1729@gmail.com>
2021-12-23 20:22:10 +0530

    added update version

--- Commands/help.js
+++ Commands/help.js
@@ -1,4 +1,6 @@
 const chalk = require("chalk");
+const packageJson = require('package-json');
+const { version } = require('../package.json');
 
 module.exports = {
   command: "$0",
@@ -65,5 +67,14 @@ module.exports = {
       Array(5).fill("\xa0").join("") +
         "lighthouse-web3 import-wallet --path wallet.json\n"
     );
+    
+    const response = await packageJson('lighthouse-web3');
+    if(response){
+      console.log(chalk.yellow("Current Version: ") + version);
+      console.log(chalk.yellow("Latest Version : ") + response.version);
+      if(version!==response.version){
+        console.log(chalk.yellow("To update run  : ") + "npm i -g lighthouse-web3@latest");
+      }
+    }
   },
 };
