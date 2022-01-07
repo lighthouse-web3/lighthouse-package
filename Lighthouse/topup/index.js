48a0aba7168ce02cc3c4588100db11926d5e66eb
ravish1729 <ravish1729@gmail.com>
2022-01-07 15:18:18 +0530

    topup function

---
+++ Lighthouse/topup/index.js
@@ -0,0 +1,22 @@
+const ethers = require("ethers");
+const package_config = require("../../config.json");
+const { depositAbi } = require("../contract_abi/depositAbi.js");
+
+exports.topup = async (signer, amount, chain = "polygon", network = "testnet") => {
+  try {
+    const contract = new ethers.Contract(
+      package_config[network][chain]["deposit_contract_address"],
+      depositAbi,
+      signer
+    );
+    
+    const txResponse = await contract.addDeposit({
+      value: ethers.utils.parseEther(amount.toString()),
+    });
+    
+    const txReceipt = await txResponse.wait();
+    return(txReceipt);
+  } catch (e) {
+    return(null);
+  }
+};
