d17f4e0ef1973d0b4568d51c4bbdd3271246df86
ravish1729 <ravish1729@gmail.com>
2022-01-09 15:46:38 +0530

    deleted

--- Lighthouse/check_deposit/index.test.js
+++
@@ -1,8 +0,0 @@
-
-const { check_deposit } = require(".");
-
-test("check_deposit", async () => {
-  const deposit = await check_deposit("0x1Ec09D4B3Cb565b7CCe2eEAf71CC90c9b46c5c26", "polygon", "testnet");
-  
-  expect(typeof deposit).toBe("number");
-}, 30000);
