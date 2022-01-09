8011ae75dc26cadb3feab36ed08c01686a8a5bcf
ravish1729 <ravish1729@gmail.com>
2022-01-09 15:16:38 +0530

    url update

--- Lighthouse/list_data/index.js
+++ Lighthouse/list_data/index.js
@@ -4,12 +4,12 @@ const config = require("../../config.json");
 exports.list_data = async (offset, limit) => {
   if (offset !== undefined && limit !== undefined) {
     const response = await axios.get(
-      config.URL + `/api/estuary/list_data?offset=${offset}&limit=${limit}`
+      config.URL + `/api/lighthouse/list_data?offset=${offset}&limit=${limit}`
     );
     return response.data;
   } else {
     const response = await axios.get(
-      config.URL + "/api/estuary/list_data?offset=0&limit=2"
+      config.URL + "/api/lighthouse/list_data?offset=0&limit=2"
     );
     return response.data;
   }
