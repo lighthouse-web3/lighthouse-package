aa4f37bda787e5d7ac8d61e8c2c478933fb835af
ravish1729 <ravish1729@gmail.com>
2022-01-09 15:20:19 +0530

    lint

--- Lighthouse/topup/index.js
+++ Lighthouse/topup/index.js
@@ -2,21 +2,26 @@ const ethers = require("ethers");
 const package_config = require("../../config.json");
 const { depositAbi } = require("../contract_abi/depositAbi.js");
 
-exports.topup = async (signer, amount, chain = "polygon", network = "testnet") => {
+exports.topup = async (
+  signer,
+  amount,
+  chain = "polygon",
+  network = "testnet"
+) => {
   try {
     const contract = new ethers.Contract(
       package_config[network][chain]["deposit_contract_address"],
       depositAbi,
       signer
     );
-    
+
     const txResponse = await contract.addDeposit({
       value: ethers.utils.parseEther(amount.toString()),
     });
-    
+
     const txReceipt = await txResponse.wait();
-    return(txReceipt);
+    return txReceipt;
   } catch (e) {
-    return(null);
+    return null;
   }
 };
