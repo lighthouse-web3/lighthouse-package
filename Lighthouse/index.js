7e23fc1d3c741f0568fc424cf1aa782bb64b5eec
ravish1729 <ravish1729@gmail.com>
2022-02-02 21:01:41 +0530

    browser support modification

--- Lighthouse/index.js
+++ Lighthouse/index.js
@@ -1,14 +1,15 @@
-const deploy = require("./deploy");
 const create_wallet = require("./create_wallet");
 const get_balance = require("./get_balance");
 const get_deals = require("./get_deals");
 const get_key = require("./get_key");
-const get_quote = require("./get_quote");
 const list_data = require("./list_data");
 const status = require("./status");
 const restore_keys = require("./restore_keys");
 
-if (typeof window === 'undefined') {
+if (typeof window === "undefined") {
+  const deploy = require("./deploy");
+  const get_quote = require("./get_quote");
+
   module.exports = {
     deploy,
     create_wallet,
@@ -20,10 +21,15 @@ if (typeof window === 'undefined') {
     status,
     restore_keys,
   };
-} else{
+} else {
+  const deploy = require("./deploy/browser");
+  const get_quote = require("./get_quote/browser");
+
   module.exports = {
+    deploy,
     create_wallet,
     get_key,
+    get_quote,
     get_deals,
     get_balance,
     list_data,
@@ -31,4 +37,3 @@ if (typeof window === 'undefined') {
     restore_keys,
   };
 }
-
