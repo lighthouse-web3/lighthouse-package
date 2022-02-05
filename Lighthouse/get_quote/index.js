2bfbabef3eeb7a7435dd40c0b215d48689a1190c
ravish1729 <ravish1729@gmail.com>
2022-02-05 20:24:09 +0530

    improving implementation and response

--- Lighthouse/get_quote/index.js
+++ Lighthouse/get_quote/index.js
@@ -1,9 +1,9 @@
-// const fs = require("fs");
 const axios = require("axios");
-// const mime = require("mime-types");
+const ethers = require("ethers");
+
 const Hash = require("../get_hash");
-// const { resolve, relative, join } = require("path");
-const config = require("../../lighthouse.config");
+const lighthouse_config = require("../../lighthouse.config");
+const { lighthouseAbi } = require("../contract_abi/lighthouseAbi.js");
 
 const getAllFiles = (
   resolve,
@@ -51,6 +51,152 @@ const getAllFiles = (
   return arrayOfFiles;
 };
 
+const get_cid = async(path, publicKey, chain, network) =>{
+  const { resolve, relative, join } = eval("require")("path");
+  const fs = eval("require")("fs");
+  const mime = eval("require")("mime-types");
+
+  if (fs.lstatSync(path).isDirectory()) {
+
+    // Get metadata and cid for all files
+    const sources = getAllFiles(resolve, relative, join, fs, path);
+    const meta_data = [];
+    const hash_list = [];
+    let total_size = 0;
+
+    for (let i = 0; i < sources.length; i++) {
+      try {
+        const readStream = fs.createReadStream(sources[i].path);
+        const stats = fs.statSync(sources[i].path);
+        const mime_type = mime.lookup(sources[i].path);
+        const fileSizeInBytes = stats.size;
+        const file_name = sources[i].path.split("/").pop();
+
+        total_size += fileSizeInBytes;
+
+        const cid = await Hash.of(readStream, {
+          cidVersion: 1,
+          rawLeaves: true,
+          chunker: "rabin",
+          minChunkSize: 1048576,
+        });
+
+        meta_data.push({
+          file_size: fileSizeInBytes,
+          mime_type: mime_type,
+          file_name: file_name,
+          cid: cid,
+        });
+
+        hash_list.push(cid);
+      } catch (err) {
+        continue;
+      }
+    }
+
+    // Get ticker for the given currency 
+    const response = await axios.get(
+      lighthouse_config.URL + `/api/lighthouse/get_ticker?symbol=${lighthouse_config["mainnet"][chain]["symbol"]}`
+    );
+    const token_price_usd = response.data;
+
+    // Get cost of each file and total cost
+    for (let i = 0; i < meta_data.length; i++) {
+      const fileSize = meta_data[i].file_size / (1024 * 1024 * 1024);
+      const cost_usd = fileSize * 5;
+      const file_cost = cost_usd / token_price_usd;
+      meta_data[i].cost = file_cost;
+    }
+
+    const totalSize = total_size / (1024 * 1024 * 1024);
+    const total_cost_usd = totalSize * 5;
+    const total_cost = total_cost_usd / token_price_usd;
+
+    // Get current balance
+    const provider = new ethers.providers.JsonRpcProvider(
+      lighthouse_config[network][chain]["rpc"]
+    );
+    const current_balance = await provider.getBalance(publicKey);
+
+    // Estimate gas fee
+    const contract = new ethers.Contract(
+      lighthouse_config[network][chain]["lighthouse_contract_address"],
+      lighthouseAbi,
+      provider
+    );
+    const gasFee = (
+      await contract.estimateGas.store(hash_list[hash_list.length - 1], {})
+    ).toNumber();
+
+    // Return data
+    return {
+      meta_data: meta_data,
+      current_balance: current_balance,
+      gasFee: gasFee,
+      total_size: total_size,
+      total_cost: total_cost,
+    };
+  } else {
+    const stats = fs.statSync(path);
+    const mime_type = mime.lookup(path);
+    const fileSizeInBytes = stats.size;
+    const file_name = path.split("/").pop();
+
+    const readStream = fs.createReadStream(path);
+    const cid = await Hash.of(readStream, {
+      cidVersion: 1,
+      rawLeaves: true,
+      chunker: "rabin",
+      minChunkSize: 1048576,
+    });
+
+    // Get ticker for the given currency 
+    const response = await axios.get(
+      lighthouse_config.URL + `/api/lighthouse/get_ticker?symbol=${lighthouse_config["mainnet"][chain]["symbol"]}`
+    );
+    const token_price_usd = response.data;
+
+    // Get cost of file
+    const totalSize = fileSizeInBytes / (1024 * 1024 * 1024);
+    const total_cost_usd = totalSize * 5;
+    const total_cost = total_cost_usd / token_price_usd;
+
+    // Get current balance
+    const provider = new ethers.providers.JsonRpcProvider(
+      lighthouse_config[network][chain]["rpc"]
+    );
+    const current_balance = await provider.getBalance(publicKey);
+
+    // Estimate gas fee
+    const contract = new ethers.Contract(
+      lighthouse_config[network][chain]["lighthouse_contract_address"],
+      lighthouseAbi,
+      provider
+    );
+    const gasFee = (
+      await contract.estimateGas.store(cid, {})
+    ).toNumber();
+
+    // return response data
+    const meta_data = [
+      {
+        file_size: fileSizeInBytes,
+        mime_type: mime_type,
+        file_name: file_name,
+        cid: cid,
+        cost: total_cost,
+      },
+    ];
+    return {
+      meta_data: meta_data,
+      gasFee: gasFee,
+      current_balance: current_balance,
+      total_size: fileSizeInBytes,
+      total_cost: total_cost,
+    };
+  }
+}
+
 module.exports = async (
   path,
   publicKey,
@@ -58,119 +204,8 @@ module.exports = async (
   network = "testnet"
 ) => {
   try {
-    const { resolve, relative, join } = eval("require")("path");
-    const fs = eval("require")("fs");
-    const mime = eval("require")("mime-types");
-    if (fs.lstatSync(path).isDirectory()) {
-      const sources = getAllFiles(resolve, relative, join, fs, path);
-      const meta_data = [];
-      const hash_list = [];
-      let total_size = 0;
-
-      for (let i = 0; i < sources.length; i++) {
-        try {
-          const readStream = fs.createReadStream(sources[i].path);
-          const stats = fs.statSync(sources[i].path);
-          const mime_type = mime.lookup(sources[i].path);
-          const fileSizeInBytes = stats.size;
-          const file_name = sources[i].path.split("/").pop();
-          total_size += fileSizeInBytes;
-
-          const ipfs_hash = await Hash.of(readStream, {
-            cidVersion: 1,
-            rawLeaves: true,
-            chunker: "rabin",
-            minChunkSize: 1048576,
-          });
-          meta_data.push({
-            file_size: fileSizeInBytes,
-            mime_type: mime_type,
-            file_name: file_name,
-            ipfs_hash: ipfs_hash,
-          });
-          hash_list.push(ipfs_hash);
-        } catch (err) {
-          continue;
-        }
-      }
-
-      const body = {
-        fileSize: total_size,
-        publicKey: publicKey,
-        ipfs_hash: hash_list.toString(),
-        chain: chain,
-        network: network,
-      };
-
-      const response = await axios.post(
-        config.URL + `/api/lighthouse/get_quote`,
-        body
-      );
-
-      for (let i = 0; i < meta_data.length; i++) {
-        meta_data[i].cost = response.data.cost;
-      }
-
-      return {
-        meta_data: meta_data,
-        hash_list: hash_list.toString(),
-        gasFee: response.data.gasFee,
-        current_balance: response.data.current_balance,
-        total_size: total_size,
-        total_cost: response.data.cost,
-      };
-    } else {
-      const stats = fs.statSync(path);
-      const mime_type = mime.lookup(path);
-      const fileSizeInBytes = stats.size;
-      const file_name = path.split("/").pop();
-
-      const readStream = fs.createReadStream(path);
-      const ipfs_hash = await Hash.of(readStream, {
-        cidVersion: 1,
-        rawLeaves: true,
-        chunker: "rabin",
-        minChunkSize: 1048576,
-      });
-
-      const body = {
-        fileSize: fileSizeInBytes,
-        publicKey: publicKey,
-        ipfs_hash: ipfs_hash,
-        chain: chain,
-        network: network,
-      };
-      const response = await axios.post(
-        config.URL + `/api/lighthouse/get_quote`,
-        body
-      );
-
-      response.data.file_size = fileSizeInBytes;
-      response.data.mime_type = mime_type;
-      response.data.file_name = file_name;
-      response.data.ipfs_hash = ipfs_hash;
-
-      // return response.data;
-      const meta_data = [
-        {
-          file_size: fileSizeInBytes,
-          mime_type: mime_type,
-          file_name: file_name,
-          ipfs_hash: ipfs_hash,
-          cost: response.data.cost,
-        },
-      ];
-      return {
-        meta_data: meta_data,
-        hash_list: [ipfs_hash].toString(),
-        gasFee: response.data.gasFee,
-        current_balance: response.data.current_balance,
-        total_size: response.data.file_size,
-        total_cost: response.data.cost,
-      };
-    }
+    return (await get_cid(path, publicKey, chain, network));    
   } catch (err) {
-    console.log(err);
     return null;
   }
 };
