f8e6669768a187a7a6ff1f971f4c9cd8c98b6f0a
ravish1729 <ravish1729@gmail.com>
2022-02-08 15:34:40 +0530

    added error handling

--- Commands/get-uploads.js
+++ Commands/get-uploads.js
@@ -15,25 +15,29 @@ module.exports = {
         chalk.green("Description: ") + "Get details of file uploaded"
       );
     } else {
-      if (config.get("Lighthouse_publicKey")) {
-        const chain = config.get("Lighthouse_chain")
-          ? config.get("Lighthouse_chain")
-          : "polygon";
-        const current_network = config.get("Lighthouse_network")
-          ? config.get("Lighthouse_network")
-          : "mainnet";
-
-        const response = await lighthouse.get_uploads(
-          config.get("Lighthouse_publicKey"),
-          chain,
-          current_network
-        );
-        console.log(chalk.yellow("CID: "));
-        for (let i = 0; i < response.length; i++) {
-          console.log(Array(5).fill("\xa0").join("") + response[i]["cid"]);
+      try{
+        if (config.get("Lighthouse_publicKey")) {
+          const chain = config.get("Lighthouse_chain")
+            ? config.get("Lighthouse_chain")
+            : "polygon";
+          const current_network = config.get("Lighthouse_network")
+            ? config.get("Lighthouse_network")
+            : "mainnet";
+  
+          const response = await lighthouse.get_uploads(
+            config.get("Lighthouse_publicKey"),
+            chain,
+            current_network
+          );
+          console.log(chalk.yellow("CID: "));
+          for (let i = 0; i < response.length; i++) {
+            console.log(Array(5).fill("\xa0").join("") + response[i]["cid"]);
+          }
+        } else {
+          console.log(chalk.red("Please import wallet first!"));
         }
-      } else {
-        console.log(chalk.red("Please import wallet first!"));
+      } catch{
+        console.log(chalk.red("Something went wrong!"));
       }
     }
   },
