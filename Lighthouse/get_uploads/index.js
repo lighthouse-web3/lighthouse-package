32cffef1296c408951d87c484556e0f6a62f3b12
ravish1729 <ravish1729@gmail.com>
2022-02-23 23:19:05 +0530

    error handling

--- Lighthouse/get_uploads/index.js
+++ Lighthouse/get_uploads/index.js
@@ -2,9 +2,13 @@ const axios = require("axios");
 const lighthouse_config = require("../../lighthouse.config");
 
 module.exports = async (publicKey, network = "polygon-testnet") => {
-  const response = await axios.get(
-    lighthouse_config.URL +
-      `/api/lighthouse/get_uploads?network=${network}&publicKey=${publicKey}`
-  );
-  return response.data;
+  try {
+    const response = await axios.get(
+      lighthouse_config.URL +
+        `/api/lighthouse/get_uploads?network=${network}&publicKey=${publicKey}`
+    );
+    return response.data;
+  } catch {
+    return null;
+  }
 };
