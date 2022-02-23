991073e84f4122006b09fbc06123779dd2b3d03e
ravish1729 <ravish1729@gmail.com>
2022-02-23 22:40:58 +0530

    modified code with better practice

--- Commands/get-uploads.js
+++ Commands/get-uploads.js
@@ -1,28 +1,40 @@
 const chalk = require("chalk");
+const Conf = require("conf");
 
 const getNetwork = require("./Utils/getNetwork");
 const lighthouse = require("../Lighthouse");
+
+const config = new Conf();
 
 module.exports = {
   command: "get-uploads",
   desc: "Get details of file uploaded",
   handler: async function (argv) {
     if (argv.help) {
-      console.log("lighthouse-web3 get-uploads\n" + chalk.green("Description: ") + "Get details of file uploaded");
+      console.log(
+        "lighthouse-web3 get-uploads\n" +
+          chalk.green("Description: ") +
+          "Get details of file uploaded"
+      );
     } else {
-      if(getNetwork() && config.get("LIGHTHOUSE_GLOBAL_PUBLICKEY")){
-        const response = await lighthouse.get_uploads(
-          config.get("LIGHTHOUSE_GLOBAL_PUBLICKEY"),
-          network
-        );
+      const network = getNetwork();
 
-        console.log(chalk.yellow("CID: "));
-        for (let i = 0; i < response.length; i++) {
-          console.log(Array(5).fill("\xa0").join("") + response[i]["cid"]);
+      network && config.get("LIGHTHOUSE_GLOBAL_PUBLICKEY")?
+      (async () => {
+        try{
+          const response = await lighthouse.get_uploads(
+            config.get("LIGHTHOUSE_GLOBAL_PUBLICKEY"),
+            network
+          );
+  
+          console.log(chalk.yellow("CID: "));
+          for (let i = 0; i < response.length; i++) {
+            console.log(Array(5).fill("\xa0").join("") + response[i]["cid"]);
+          }
+        } catch{
+          console.log(chalk.red("Error fetching uploads!"));
         }
-      } else{
-        console.log(chalk.red("You have not imported wallet or selected the network!"));
-      }
+      })() : console.log(chalk.red("You have not imported wallet or selected the network!"));
     }
-  },
+  }
 };
