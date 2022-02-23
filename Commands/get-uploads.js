d1c632d1d86bb7dbeae42ee0e9ae083f6c1410cb
ravish1729 <ravish1729@gmail.com>
2022-02-23 22:09:07 +0530

    modified code with better practices

--- Commands/get-uploads.js
+++ Commands/get-uploads.js
@@ -1,38 +1,27 @@
 const chalk = require("chalk");
-const Conf = require("conf");
 
+const getNetwork = require("./Utils/getNetwork");
 const lighthouse = require("../Lighthouse");
-const config = new Conf();
 
 module.exports = {
   command: "get-uploads",
   desc: "Get details of file uploaded",
   handler: async function (argv) {
     if (argv.help) {
-      console.log("lighthouse-web3 get-uploads\n");
-      console.log(
-        chalk.green("Description: ") + "Get details of file uploaded"
-      );
+      console.log("lighthouse-web3 get-uploads\n" + chalk.green("Description: ") + "Get details of file uploaded");
     } else {
-      try {
-        if (config.get("Lighthouse_publicKey")) {
-          const network = config.get("Lighthouse_network")
-            ? config.get("Lighthouse_network")
-            : "polygon";
+      if(getNetwork() && config.get("LIGHTHOUSE_GLOBAL_PUBLICKEY")){
+        const response = await lighthouse.get_uploads(
+          config.get("LIGHTHOUSE_GLOBAL_PUBLICKEY"),
+          network
+        );
 
-          const response = await lighthouse.get_uploads(
-            config.get("Lighthouse_publicKey"),
-            network
-          );
-          console.log(chalk.yellow("CID: "));
-          for (let i = 0; i < response.length; i++) {
-            console.log(Array(5).fill("\xa0").join("") + response[i]["cid"]);
-          }
-        } else {
-          console.log(chalk.red("Please import wallet first!"));
+        console.log(chalk.yellow("CID: "));
+        for (let i = 0; i < response.length; i++) {
+          console.log(Array(5).fill("\xa0").join("") + response[i]["cid"]);
         }
-      } catch {
-        console.log(chalk.red("Something went wrong!"));
+      } else{
+        console.log(chalk.red("You have not imported wallet or selected the network!"));
       }
     }
   },
