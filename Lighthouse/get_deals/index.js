a2ce2f17caebb318fb5442b17cbd7d718e2055cb
ravish1729 <ravish1729@gmail.com>
2022-02-25 12:07:40 +0530

    not used

--- Lighthouse/get_deals/index.js
+++
@@ -1,9 +0,0 @@
-const axios = require("axios");
-const config = require("../../lighthouse.config");
-
-module.exports = async (content_id) => {
-  const response = await axios.get(
-    config.URL + `/api/lighthouse/get_deals?content_id=${content_id}`
-  );
-  return response.data;
-};
