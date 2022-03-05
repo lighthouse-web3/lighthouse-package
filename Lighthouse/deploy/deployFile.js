af27ec2994b6c30ea2d253a97a67bb9c2f819535
ravish1729 <ravish1729@gmail.com>
2022-03-06 00:58:06 +0530

    function from deploy added to seperate files

---
+++ Lighthouse/deploy/deployFile.js
@@ -0,0 +1,100 @@
+const axios = require("axios");
+
+/*
+  This function is used to deploy a file to the Lighthouse server.
+  It takes the following parameters:
+  @param {string} sourcePath - The path of file/folder.
+  @param {string} publicKey - The public key of the user.
+  @param {string} signedMessage - The signed message for verification.
+*/
+
+module.exports = (sourcePath, publicKey, signedMessage) => {
+  const fs = eval("require")("fs");
+  const NodeFormData = eval("require")("form-data");
+  const recursive = eval("require")("recursive-fs");
+  const basePathConverter = eval("require")("base-path-converter");
+  const token = "Bearer " + publicKey + " " + signedMessage;
+
+  return new Promise((resolve, reject) => {
+    const endpoint = `https://node.lighthouse.storage/api/v0/add`;
+
+    fs.stat(sourcePath, (err, stats) => {
+      if (err) {
+        reject(err);
+      }
+      if (stats.isFile()) {
+        //we need to create a single read stream instead of reading the directory recursively
+        const data = new NodeFormData();
+
+        data.append("file", fs.createReadStream(sourcePath));
+
+        axios
+          .post(endpoint, data, {
+            withCredentials: true,
+            maxContentLength: "Infinity", //this is needed to prevent axios from erroring out with large directories
+            maxBodyLength: "Infinity",
+            headers: {
+              "Content-type": `multipart/form-data; boundary= ${data._boundary}`,
+              Authorization: token,
+            },
+          })
+          .then(function (result) {
+            if (result.status !== 200) {
+              reject(
+                new Error(
+                  `unknown server response while pinning File to IPFS: ${result}`
+                )
+              );
+            }
+            resolve(result.data);
+          })
+          .catch(function (error) {
+            console.log(error);
+            reject(formattedError);
+          });
+      } else {
+        recursive.readdirr(sourcePath, function (err, dirs, files) {
+          if (err) {
+            reject(new Error(err));
+          }
+
+          let data = new NodeFormData();
+
+          files.forEach((file) => {
+            //for each file stream, we need to include the correct relative file path
+            data.append("file", fs.createReadStream(file), {
+              filepath: basePathConverter(sourcePath, file),
+            });
+          });
+
+          axios
+            .post(endpoint, data, {
+              withCredentials: true,
+              maxContentLength: "Infinity",
+              maxBodyLength: "Infinity", //this is needed to prevent axios from erroring out with large directories
+              headers: {
+                "Content-type": `multipart/form-data; boundary= ${data._boundary}`,
+                Authorization: token,
+              },
+            })
+            .then(function (result) {
+              if (result.status !== 200) {
+                reject(
+                  new Error(
+                    `unknown server response while pinning File to IPFS: ${result}`
+                  )
+                );
+              }
+
+              const temp = result.data.split("\n");
+              const response = JSON.parse(temp[temp.length - 2]);
+              resolve(response);
+            })
+            .catch(function (error) {
+              reject(error);
+            });
+        });
+      }
+    });
+  });
+}
---
+++ Lighthouse/deploy/getCost.js
@@ -0,0 +1,27 @@
+const axios = require("axios");
+
+const lighthouseConfig = require("../../lighthouse.config");
+
+/*
+  This function is used to deploy a file to the Lighthouse server.
+  It takes the following parameters:
+  @param {string} fileSize - The size of file/folder.
+  @param {string} network - Network on which transaction to execute.
+*/
+
+module.exports = async (fileSize, network) => {
+  // Get ticker for the given currency
+  const tokenPriceUsd = (
+    await axios.get(
+      lighthouseConfig.URL +
+        `/api/lighthouse/get_ticker?symbol=${lighthouseConfig[network]["symbol"]}`
+    )
+  ).data;
+
+  // Get cost of file
+  const totalSize = fileSize / lighthouseConfig.gbInBytes;
+  const totalCostUsd = totalSize * lighthouseConfig.costPerGB;
+  const totalCost = (totalCostUsd / tokenPriceUsd).toFixed(18);
+
+  return totalCost;
+};
