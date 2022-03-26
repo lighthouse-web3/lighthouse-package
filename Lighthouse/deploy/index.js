3473afdc3c5cbcef72ea7d9726fb8babc030183c
ravish1729 <ravish1729@gmail.com>
2022-03-26 14:20:05 +0530

    updated with apiKey, pushing cid moved to a seperate file

--- Lighthouse/deploy/index.js
+++ Lighthouse/deploy/index.js
@@ -1,80 +1,10 @@
-const chalk = require("chalk");
-
-const addCid = require("../addCid");
 const deployFile = require("./deployFile");
-const getCost = require("./getCost");
-const pushCidToChain = require("./pushCidToChain");
-const lighthouseConfig = require("../../lighthouse.config");
-
-const transactionLog = (txObj, network) => {
-  const networkConfig = lighthouseConfig[network];
-
-  if (!networkConfig) {
-    console.error(`No network found for ${network}`);
-  }
-
-  if (txObj) {
-    console.log("Transaction: " + networkConfig.scan + txObj.transactionHash);
-  } else {
-    console.log("Transaction failed");
-  }
-};
 
 module.exports = async (
   path,
-  signer,
-  cli = false,
-  signedMessage,
-  publicKey,
-  network
+  apiKey,
+  publicKey
 ) => {
   // Upload File to IPFS
-  const Spinner = eval("require")("cli-spinner").Spinner;
-  let spinner = new Spinner("Uploading File");
-  if (cli) {
-    spinner.start();
-  }
-
-  let deployResponse = await deployFile(path, publicKey, signedMessage);
-
-  if (cli) {
-    spinner.stop();
-    process.stdout.clearLine();
-    process.stdout.cursorTo(0);
-  }
-
-  // Push CID to chain
-  if (deployResponse) {
-    spinner = new Spinner();
-    if (cli) {
-      console.log(chalk.green("Pushing CID to chain"));
-      spinner.start();
-    }
-
-    const cost = await getCost(deployResponse.Size, network);
-    const txObj = await pushCidToChain(
-      signer,
-      deployResponse.Hash,
-      deployResponse.Name,
-      deployResponse.Size,
-      cost.toString(),
-      network
-    );
-
-    if (cli) {
-      spinner.stop();
-      process.stdout.clearLine();
-      process.stdout.cursorTo(0);
-
-      transactionLog(txObj, network);
-
-      console.log(chalk.green("CID pushed to chain"));
-    }
-
-    const temp = await addCid(deployResponse.Name, deployResponse.Hash);
-
-    deployResponse["txObj"] = txObj;
-    return deployResponse;
-  } else {
-  }
+  return(await deployFile(path, publicKey, apiKey));
 };
