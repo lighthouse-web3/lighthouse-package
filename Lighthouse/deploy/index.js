cadf31258d99fbeb933e913b9325ae924a963bba
ravish1729 <ravish1729@gmail.com>
2022-03-26 14:35:21 +0530

    lint

--- Lighthouse/deploy/index.js
+++ Lighthouse/deploy/index.js
@@ -1,10 +1,6 @@
 const deployFile = require("./deployFile");
 
-module.exports = async (
-  path,
-  apiKey,
-  publicKey
-) => {
+module.exports = async (path, apiKey, publicKey) => {
   // Upload File to IPFS
-  return(await deployFile(path, publicKey, apiKey));
+  return await deployFile(path, publicKey, apiKey);
 };
