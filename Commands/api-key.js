8b1c56574dcce568d65163660c6271be297c562a
ravish1729 <ravish1729@gmail.com>
2022-03-26 14:30:37 +0530

    modified help section

--- Commands/api-key.js
+++ Commands/api-key.js
@@ -14,10 +14,13 @@ module.exports = {
       console.log(
         "lighthouse-web3 api-key\n" +
           chalk.green("Description: ") +
-          "Get new api key or show current key\n"
+          "Get new api key or show current key\n" +
+          chalk.green("Options: \n") +
+          Array(3).fill("\xa0").join("") +
+          "- new: To generate a new api key\n"
       );
     } else {
-      if (argv.new || !config.get("LIGHTHOUSE_GLOBAL_API_KEY")){
+      if (argv.new || !config.get("LIGHTHOUSE_GLOBAL_API_KEY")) {
         const options = {
           prompt: "Enter your password: ",
           silent: true,
@@ -29,16 +32,20 @@ module.exports = {
           password.trim()
         );
 
-        key? (async () => {
-          const apiKey = await lighthouse.getApiKey(key.privateKey, true);
-          config.set("LIGHTHOUSE_GLOBAL_API_KEY", apiKey);
-          console.log(chalk.yellow("\nApi Key: ") + apiKey);
-        })() : (()=>{
-          console.log(chalk.red("Incorrect password!"));
-          process.exit();
-        })() 
-      } else{
-        console.log(chalk.yellow("\nApi Key ") + config.get("LIGHTHOUSE_GLOBAL_API_KEY"));
+        key
+          ? (async () => {
+              const apiKey = await lighthouse.getApiKey(key.privateKey, true);
+              config.set("LIGHTHOUSE_GLOBAL_API_KEY", apiKey);
+              console.log(chalk.yellow("\nApi Key: ") + apiKey);
+            })()
+          : (() => {
+              console.log(chalk.red("Incorrect password!"));
+              process.exit();
+            })();
+      } else {
+        console.log(
+          chalk.yellow("\nApi Key ") + config.get("LIGHTHOUSE_GLOBAL_API_KEY")
+        );
       }
     }
   },
