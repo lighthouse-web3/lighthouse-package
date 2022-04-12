ba5fbdec87e55cabbc5f3b2364a5a80676ef1f25
ravish <ravish1729@gmail.com>
2022-04-12 08:23:01 +0530

    updated the request

--- Lighthouse/getApiKey/index.js
+++ Lighthouse/getApiKey/index.js
@@ -1,23 +1,17 @@
 const axios = require("axios");
-const ethers = require("ethers");
 
 const lighthouseConfig = require("../../lighthouse.config");
 
-module.exports = async (privateKey, newKey) => {
+module.exports = async (publicKey, signedMessage) => {
   try {
-    const provider = new ethers.getDefaultProvider();
-    const signer = new ethers.Wallet(privateKey, provider);
-    const publicKey = await signer.getAddress();
-    const message = (
-      await axios.get(
-        lighthouseConfig.URL +
-          `/api/lighthouse/get_message?publicKey=${publicKey}&new=${newKey}`
-      )
+    const apiKey = (
+      await axios.post(lighthouseConfig.URL + `/api/auth/get_api_key`, {
+        publicKey: publicKey,
+        signedMessage: signedMessage,
+      })
     ).data;
-    const apiKey = await signer.signMessage(message);
     return apiKey;
-  } catch (e) {
-    console.log(e);
+  } catch (error) {
     return null;
   }
 };
