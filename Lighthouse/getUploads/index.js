7dd57f9520793c361300a68ce2fe5f367bc81db2
ravish1729 <ravish1729@gmail.com>
2022-04-12 14:24:05 +0530

    endpoint updated

--- Lighthouse/getUploads/index.js
+++ Lighthouse/getUploads/index.js
@@ -1,12 +1,12 @@
 const axios = require("axios");
 const lighthouseConfig = require("../../lighthouse.config");
 
-module.exports = async (publicKey, network) => {
+module.exports = async (publicKey) => {
   try {
     const uploads = (
       await axios.get(
         lighthouseConfig.URL +
-          `/api/lighthouse/get_uploads?network=${network}&publicKey=${publicKey}`
+          `/api/user/get_uploads?publicKey=${publicKey}`
       )
     ).data;
     return uploads;
