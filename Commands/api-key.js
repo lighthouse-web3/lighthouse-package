797a7394da462f451ec0f560739626f0f8680062
ravish <ravish1729@gmail.com>
2022-04-12 08:22:20 +0530

    command to get api-key

--- Commands/api-key.js
+++ Commands/api-key.js
@@ -1,52 +1,58 @@
+const axios = require("axios");
 const Conf = require("conf");
 const chalk = require("chalk");
+const ethers = require("ethers");
 
 const lighthouse = require("../Lighthouse");
 const readInput = require("./Utils/readInput");
+const lighthouseConfig = require("../lighthouse.config");
 
 const config = new Conf();
 
 module.exports = {
   command: "api-key",
-  desc: "Get new api key or show current key",
+  desc: "Get a new api key",
   handler: async function (argv) {
     if (argv.help) {
       console.log(
         "lighthouse-web3 api-key\n" +
           chalk.green("Description: ") +
-          "Get new api key or show current key\n" +
-          chalk.green("Options: \n") +
-          Array(3).fill("\xa0").join("") +
-          "- new: To generate a new api key\n"
+          "Get a new api key\n"
       );
     } else {
-      if (argv.new || !config.get("LIGHTHOUSE_GLOBAL_API_KEY")) {
-        const options = {
-          prompt: "Enter your password: ",
-          silent: true,
-          default: "",
-        };
-        const password = await readInput(options);
-        const key = await lighthouse.getKey(
-          config.get("LIGHTHOUSE_GLOBAL_PRIVATEKEYENCRYPTED"),
-          password.trim()
-        );
+      const publicKey = config.get("LIGHTHOUSE_GLOBAL_PUBLICKEY");
+      const options = {
+        prompt: "Enter your password: ",
+        silent: true,
+        default: "",
+      };
+      const password = await readInput(options);
+      const key = await lighthouse.getKey(
+        config.get("LIGHTHOUSE_GLOBAL_PRIVATEKEYENCRYPTED"),
+        password.trim()
+      );
 
-        key
-          ? (async () => {
-              const apiKey = await lighthouse.getApiKey(key.privateKey, true);
-              config.set("LIGHTHOUSE_GLOBAL_API_KEY", apiKey);
-              console.log(chalk.yellow("\nApi Key: ") + apiKey);
-            })()
-          : (() => {
-              console.log(chalk.red("Incorrect password!"));
-              process.exit();
-            })();
-      } else {
-        console.log(
-          chalk.yellow("\nApi Key ") + config.get("LIGHTHOUSE_GLOBAL_API_KEY")
-        );
-      }
+      key
+        ? (async () => {
+            const verificationMessage = (
+              await axios.get(
+                lighthouseConfig.URL +
+                  `/api/auth/get_message?publicKey=${publicKey}`
+              )
+            ).data;
+            const provider = new ethers.getDefaultProvider();
+            const signer = new ethers.Wallet(key.privateKey, provider);
+            const signedMessage = await signer.signMessage(verificationMessage);
+
+            const apiKey = await lighthouse.getApiKey(publicKey, signedMessage);
+
+            config.set("LIGHTHOUSE_GLOBAL_API_KEY", apiKey);
+            console.log(chalk.yellow("\nApi Key: ") + apiKey);
+          })()
+        : (() => {
+            console.log(chalk.red("Incorrect password!"));
+            process.exit();
+          })();
     }
   },
 };
