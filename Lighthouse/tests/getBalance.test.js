dc17d5de4aa9b79b8dacfe08c86feb39d6dd0ed2
ravish <ravish1729@gmail.com>
2022-04-19 22:47:40 +0530

    updated

--- Lighthouse/tests/getBalance.test.js
+++ Lighthouse/tests/getBalance.test.js
@@ -1,64 +1,10 @@
 const lighthouse = require("../../Lighthouse");
 
-test("getBalance polygon-testnet", async () => {
+test("getBalance", async () => {
   const balance = await lighthouse.getBalance(
-    "0x1Ec09D4B3Cb565b7CCe2eEAf71CC90c9b46c5c26",
-    "polygon-testnet"
+    "0x487fc2fE07c593EAb555729c3DD6dF85020B5160",
   );
 
-  expect(typeof balance).toBe("string");
-}, 20000);
-
-test("getBalance fantom-testnet", async () => {
-  const balance = await lighthouse.getBalance(
-    "0x1Ec09D4B3Cb565b7CCe2eEAf71CC90c9b46c5c26",
-    "fantom-testnet"
-  );
-
-  expect(typeof balance).toBe("string");
-}, 20000);
-
-test("getBalance binance-testnet", async () => {
-  const balance = await lighthouse.getBalance(
-    "0x1Ec09D4B3Cb565b7CCe2eEAf71CC90c9b46c5c26",
-    "binance-testnet"
-  );
-
-  expect(typeof balance).toBe("string");
-}, 20000);
-
-test("getBalance polygon", async () => {
-  const balance = await lighthouse.getBalance(
-    "0x1Ec09D4B3Cb565b7CCe2eEAf71CC90c9b46c5c26",
-    "polygon"
-  );
-
-  expect(typeof balance).toBe("string");
-}, 20000);
-
-test("getBalance fantom", async () => {
-  const balance = await lighthouse.getBalance(
-    "0x1Ec09D4B3Cb565b7CCe2eEAf71CC90c9b46c5c26",
-    "fantom"
-  );
-
-  expect(typeof balance).toBe("string");
-}, 20000);
-
-test("getBalance binance", async () => {
-  const balance = await lighthouse.getBalance(
-    "0x1Ec09D4B3Cb565b7CCe2eEAf71CC90c9b46c5c26",
-    "binance"
-  );
-
-  expect(typeof balance).toBe("string");
-}, 20000);
-
-test("getBalance null case", async () => {
-  const balance = await lighthouse.getBalance(
-    "0x1Ec09D4B3Cb565b7CCe2eEAf71CC90c9b46c5c26",
-    null
-  );
-
-  expect(balance).toBe(null);
+  expect(typeof balance.dataLimit).toBe("number");
+  expect(typeof balance.dataUsed).toBe("number");
 }, 20000);
