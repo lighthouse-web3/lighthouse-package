500a914773e497d7c62b8edda9e1f78ef8cf4767
ravish <ravish1729@gmail.com>
2022-05-02 11:25:32 +0530

    error handeling

--- Lighthouse/addCid/index.js
+++ Lighthouse/addCid/index.js
@@ -2,12 +2,16 @@ const axios = require("axios");
 const lighthouseConfig = require("../../lighthouse.config");
 
 module.exports = async (fileName, cid) => {
-  const response = await axios.post(
-    lighthouseConfig.URL + `/api/lighthouse/add_cid`,
-    {
-      name: fileName,
-      cid: cid,
-    }
-  );
-  return response.data;
+  try {
+    const response = (
+      await axios.post(lighthouseConfig.URL + `/api/lighthouse/add_cid`, {
+        name: fileName,
+        cid: cid,
+      })
+    ).data;
+
+    return response;
+  } catch (error) {
+    return null;
+  }
 };
