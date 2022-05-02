9f643a16b9debecd01fa56a77140bef21a1ed23d
ravish <ravish1729@gmail.com>
2022-05-02 11:26:18 +0530

    added test case

---
+++ Lighthouse/tests/addCid.test.js
@@ -0,0 +1,17 @@
+// const lighthouse = require("..");
+
+// test("addCid main case", async () => {
+//   const response = await lighthouse.addCid(
+//     "adiyogi.jpg",
+//     "bafkreia4ruswe7ghckleh3lmpujo5asrnd7hrtu5r23zjk2robpcoend34"
+//   );
+//   expect(response).toBe("Added To Queue");
+// }, 20000);
+
+// test("addCid null", async () => {
+//   const response = await lighthouse.addCid(
+//     "adiyogi.jpg",
+//     "bafkreia4ruswe7ghckleh3lmpujo5asrnd7hrtu5r23zjk2robpcoend"
+//   );
+//   expect(response).toBe(null);
+// }, 20000);
