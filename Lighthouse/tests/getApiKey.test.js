b88979884ca7c4db90538d2f4f7a668eb984957e
ravish <ravish1729@gmail.com>
2022-05-03 09:50:00 +0530

    added test cases

--- Lighthouse/tests/getApiKey.test.js
+++ Lighthouse/tests/getApiKey.test.js
@@ -4,7 +4,7 @@ const lighthouse = require("..");
 const lighthouseConfig = require("../../lighthouse.config");
 
 test("getApiKey Main Case", async () => {
-  const publicKey = "0xA3C960B3BA29367ecBCAf1430452C6cd7516F588";
+  const publicKey = "0xEaF4E24ffC1A2f53c07839a74966A6611b8Cb8A1";
   const verificationMessage = (
     await axios.get(
       lighthouseConfig.URL + `/api/auth/get_message?publicKey=${publicKey}`
@@ -12,7 +12,7 @@ test("getApiKey Main Case", async () => {
   ).data;
   const provider = new ethers.getDefaultProvider();
   const signer = new ethers.Wallet(
-    "0x6aa0ee41fa9cf65f90c06e5db8fa2834399b59b37974b21f2e405955630d472a",
+    "0x8218aa5dbf4dbec243142286b93e26af521b3e91219583595a06a7765abc9c8b",
     provider
   );
   const signedMessage = await signer.signMessage(verificationMessage);
@@ -25,6 +25,6 @@ test("getApiKey Main Case", async () => {
 test("getApiKey Null Case", async () => {
   const publicKey = "0xA3C960B3BA29367ecBCAf1430452C6cd7516F588";
   const apiKey = await lighthouse.getApiKey(publicKey, "signedMessage");
-
-  expect(typeof apiKey).toBe(null);
+  
+  expect(apiKey).toBe(null);
 }, 60000);
