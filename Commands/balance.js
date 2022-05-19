5cc883530b0c3214803041500eb07c7392c1459a
ravish <ravish1729@gmail.com>
2022-05-19 06:25:58 +0530

    added stable coin and native currency

--- Commands/balance.js
+++ Commands/balance.js
@@ -1,10 +1,13 @@
 const Conf = require("conf");
 const chalk = require("chalk");
+const ethers = require("ethers");
 const Spinner = require("cli-spinner").Spinner;
 
 const lighthouse = require("../Lighthouse");
 const byteToSize = require("../Utils/byteToSize");
+const getNetwork = require("../Utils/getNetwork");
 
+const lighthouseConfig = require("../lighthouse.config");
 const config = new Conf();
 
 module.exports = {
@@ -47,6 +50,76 @@ module.exports = {
             chalk.yellow("\nData Remaining: ") +
             byteToSize(parseInt(balance.dataLimit) - parseInt(balance.dataUsed))
         );
+
+        const network = getNetwork();
+
+        if (
+          network === "polygon" ||
+          network === "fantom" ||
+          network === "binance" ||
+          network === "optimism"
+        ) {
+          const provider = new ethers.providers.JsonRpcProvider(
+            lighthouseConfig[network]["rpc"]
+          );
+
+          const contractABI = [
+            {
+              constant: true,
+              inputs: [{ name: "account", type: "address" }],
+              name: "balanceOf",
+              outputs: [{ name: "", type: "uint256" }],
+              payable: false,
+              stateMutability: "view",
+              type: "function",
+            },
+          ];
+
+          const contractUSDT = new ethers.Contract(
+            lighthouseConfig[network]["usdt_contract_address"],
+            contractABI,
+            provider
+          );
+          console.log(
+            chalk.yellow("\nUSDT Balance: ") +
+              Array(2).fill("\xa0").join("") +
+              ethers.utils.formatUnits(
+                await contractUSDT.balanceOf(
+                  config.get("LIGHTHOUSE_GLOBAL_PUBLICKEY")
+                ),
+                lighthouseConfig[network]["usd_contract_decimal"]
+              )
+          );
+
+          const contractUSDC = new ethers.Contract(
+            lighthouseConfig[network]["usdc_contract_address"],
+            contractABI,
+            provider
+          );
+          console.log(
+            chalk.yellow("USDC Balance: ") +
+              Array(2).fill("\xa0").join("") +
+              ethers.utils.formatUnits(
+                await contractUSDC.balanceOf(
+                  config.get("LIGHTHOUSE_GLOBAL_PUBLICKEY")
+                ),
+                lighthouseConfig[network]["usd_contract_decimal"]
+              )
+          );
+
+          console.log(
+            chalk.yellow(network) +
+              ":" +
+              Array(15 - network.length)
+                .fill("\xa0")
+                .join("") +
+              ethers.utils.formatEther(
+                await provider.getBalance(
+                  config.get("LIGHTHOUSE_GLOBAL_PUBLICKEY")
+                )
+              )
+          );
+        }
       } catch (error) {
         console.log(chalk.red(error.message));
       }
