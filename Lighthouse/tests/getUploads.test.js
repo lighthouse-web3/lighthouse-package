edc2d17c08e45310f3122dc223746a42e27256ac
ravish <ravish1729@gmail.com>
2022-06-06 06:16:26 +0530

    updated

--- Lighthouse/tests/getUploads.test.js
+++ Lighthouse/tests/getUploads.test.js
@@ -1,16 +1,13 @@
 const lighthouse = require("../../Lighthouse");
 
 test("getUploads", async () => {
-  const response = await lighthouse.getUploads(
-    "0x487fc2fE07c593EAb555729c3DD6dF85020B5160",
-    "fantom"
-  );
+  const response = await lighthouse.getUploads("0x487fc2fE07c593EAb555729c3DD6dF85020B5160");
 
   expect(typeof response[0]["cid"]).toBe("string");
 }, 20000);
 
 test("getUploads null case", async () => {
-  const response = await lighthouse.getUploads(null, null);
+  const response = await lighthouse.getUploads(null);
 
-  expect(response).toBe(null);
+  expect(typeof response).toBe("object");
 }, 20000);
