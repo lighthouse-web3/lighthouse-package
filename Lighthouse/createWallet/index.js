a696eff59778e5f804f6285d23b27df3eadbd9bf
ravish <ravish1729@gmail.com>
2022-06-22 17:05:25 +0530

    change config param name - URL

--- Lighthouse/createWallet/index.js
+++ Lighthouse/createWallet/index.js
@@ -5,10 +5,13 @@ const lighthouseConfig = require("../../lighthouse.config");
 module.exports = async (password) => {
   try {
     const wallet = ethers.Wallet.createRandom();
-    const _ = await axios.get(lighthouseConfig.URL + `/api/auth/get_message?publicKey=${wallet.address}`);
+    const _ = await axios.get(
+      lighthouseConfig.lighthouseAPI +
+        `/api/auth/get_message?publicKey=${wallet.address}`
+    );
     const encryptedWallet = await wallet.encrypt(password);
     return encryptedWallet;
-  } catch(error) {
+  } catch (error) {
     return null;
   }
 };
