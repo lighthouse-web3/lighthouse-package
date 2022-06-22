f8b18274cc9b82101790324b3710c9fd2a2e71f9
ravish <ravish1729@gmail.com>
2022-06-22 17:04:47 +0530

    change config param name - URL

--- Lighthouse/addCid/index.js
+++ Lighthouse/addCid/index.js
@@ -4,10 +4,13 @@ const lighthouseConfig = require("../../lighthouse.config");
 module.exports = async (fileName, cid) => {
   try {
     const response = (
-      await axios.post(lighthouseConfig.URL + `/api/lighthouse/add_cid`, {
-        name: fileName,
-        cid: cid,
-      })
+      await axios.post(
+        lighthouseConfig.lighthouseAPI + `/api/lighthouse/add_cid`,
+        {
+          name: fileName,
+          cid: cid,
+        }
+      )
     ).data;
 
     return response;
