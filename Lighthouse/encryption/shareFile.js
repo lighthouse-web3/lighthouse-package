928fa2a2c0fe4ec221b35be47dc0dbbc239a4d0e
ravish <ravish1729@gmail.com>
2022-07-08 16:36:40 +0530

    sharing fxn

---
+++ Lighthouse/encryption/shareFile.js
@@ -0,0 +1,41 @@
+const axios = require("axios");
+const { getKeyShades } = require("../../Utils/bls_helper");
+const lighthouseConfig = require("../../lighthouse.config");
+
+module.exports = async (
+  publicKey,
+  shareTo,
+  cid,
+  fileEncryptionKey,
+  signedMessage
+) => {
+  try {
+    // shade encryption key
+    const { idData, keyShades } = await getKeyShades(fileEncryptionKey);
+
+    // send encryption key
+    const _ = await Promise.all(
+      lighthouseConfig.lighthouseBLSNodes.map((url, index) => {
+        return axios.post(
+          url,
+          {
+            address: publicKey.toLowerCase(),
+            cid: cid,
+            index: idData[index],
+            key: keyShades[index],
+            sharedTo: [shareTo.toLowerCase()],
+          },
+          {
+            headers: {
+              Authorization: "Bearer " + signedMessage,
+            },
+          }
+        );
+      })
+    );
+
+    return("Shared");
+  } catch (error) {
+    return error.message;
+  }
+};
