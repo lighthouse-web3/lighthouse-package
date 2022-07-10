bfd33ffbabc3aab5ce8d211f36619e7bcd58ab8a
ravish <ravish1729@gmail.com>
2022-07-10 18:27:48 +0530

    correction

--- Lighthouse/encryption/fetchEncryptionKey.js
+++ Lighthouse/encryption/fetchEncryptionKey.js
@@ -4,42 +4,42 @@ const lighthouseConfig = require("../../lighthouse.config");
 const { recoverKey, randSelect } = require("../../Utils/bls_helper");
 
 module.exports = async (cid, publicKey, signedMessage) => {
-  const nodeIndexSelected = randSelect(3, 5);
-  let nodeUrl = await Promise.all(
-    nodeIndexSelected.map(
-      (elem) => lighthouseConfig.lighthouseBLSNodesRetrieval[elem]
-    )
-  );
-
-  // send encryption key
-  const sentShades = await Promise.all(
-    nodeUrl.map((url) => {
-      return axios
-        .post(
-          url,
-          {
-            address: publicKey.toLowerCase(),
-            cid: cid,
-          },
-          {
-            headers: {
-              Authorization: "Bearer " + signedMessage,
-            },
-          }
-        )
-        .then((res) => {
-          return res.data;
-        });
-    })
-  );
-
-  const keys = sentShades.map((elem) => elem?.key);
-  const indexes = sentShades.map((elem) => elem?.index);
-
   try {
+    const nodeIndexSelected = randSelect(3, 5);
+    let nodeUrl = await Promise.all(
+      nodeIndexSelected.map(
+        (elem) => lighthouseConfig.lighthouseBLSNodesRetrieval[elem]
+      )
+    );
+
+    // send encryption key
+    const sentShades = await Promise.all(
+      nodeUrl.map((url) => {
+        return axios
+          .post(
+            url,
+            {
+              address: publicKey.toLowerCase(),
+              cid: cid,
+            },
+            {
+              headers: {
+                Authorization: "Bearer " + signedMessage,
+              },
+            }
+          )
+          .then((res) => {
+            return res.data;
+          });
+      })
+    );
+
+    const keys = sentShades.map((elem) => elem?.key);
+    const indexes = sentShades.map((elem) => elem?.index);
+
     const key = await recoverKey(keys, indexes);
     return key;
   } catch {
-    return "insufficient Access";
+    return null;
   }
 };
