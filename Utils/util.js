2a05026524b24b900bfacd97921bb0eb3134e798
ravish <ravish1729@gmail.com>
2022-08-10 10:57:37 +0530

    bug fix

--- Utils/util.js
+++ Utils/util.js
@@ -8,11 +8,13 @@ const isCID = (cid) => {
   }
   return true;
 };
+
 const isAddress = (address) => {
-  return !ethers.utils.isAddress(address);
+  return ethers.utils.isAddress(address);
 };
 
 const isPrivateKey = (key) => {
   return /^([0-9a-f]{64})$/i.test(key);
 };
+
 module.exports = { isAddress, isCID, isPrivateKey };
