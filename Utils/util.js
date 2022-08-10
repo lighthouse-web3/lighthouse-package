5f38dbd39c43d433ee98cd5b019726bb52903889
xlassix <46370698+xlassix@users.noreply.github.com>
2022-08-10 03:47:22 +0100

    fix: isCID

--- Utils/util.js
+++ Utils/util.js
@@ -1,12 +1,10 @@
 const ethers = require("ethers");
 
 const isCID = (cid) => {
-  if (
-    cid.startsWith("Qm")
-  ) {
-    return (/^[A-HJ-NP-Za-km-z1-9]*$/.test(cid) && cid.length == 46);
-  } else if (cid.length <= 50 || !cid.startsWith("b")) {
-    return false;
+  if (cid.startsWith("Qm")) {
+    return /^[A-HJ-NP-Za-km-z1-9]*$/.test(cid) && cid.length == 46;
+  } else if (cid.startsWith("b")) {
+    return cid.length >= 50;
   }
   return true;
 };
@@ -15,6 +13,6 @@ const isAddress = (address) => {
 };
 
 const isPrivateKey = (key) => {
-  return /^([0-9a-f]{64})$/i.test(key)
+  return /^([0-9a-f]{64})$/i.test(key);
 };
-module.exports = { isAddress, isCID,isPrivateKey };
+module.exports = { isAddress, isCID, isPrivateKey };
