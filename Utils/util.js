6a6dd2e233621fcf331ba2e0278641b642067952
xlassix <46370698+xlassix@users.noreply.github.com>
2022-12-28 15:44:48 +0100

    fix: cid regex

--- Utils/util.js
+++ Utils/util.js
@@ -1,12 +1,9 @@
 const ethers = require("ethers");
-const { CID } = require('multiformats/cid');
 
-const isCID = (hash) => {
-  try {
-    return Boolean(CID.parse(hash));
-  } catch {
-    return false;
-  }
+const isCID = (cid) => {
+  return /^(Qm[1-9A-HJ-NP-Za-km-z]{44}|b[A-Za-z2-7]{58}|B[A-Z2-7]{58}|z[1-9A-HJ-NP-Za-km-z]{48}|F[0-9A-F]{50})*$/.test(
+    cid
+  );
 };
 
 const isPrivateKey = (key) => {
