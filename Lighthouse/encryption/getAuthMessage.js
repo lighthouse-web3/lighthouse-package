129dbf5393991727c710ff57bf75b1a9e51e4c57
ravish <ravish1729@gmail.com>
2022-12-29 06:03:29 +0530

    error handling

--- Lighthouse/encryption/getAuthMessage.js
+++ Lighthouse/encryption/getAuthMessage.js
@@ -2,22 +2,18 @@ const { addressValidator } = require("../../Utils/util");
 const { getAuthMessage } = require("@lighthouse-web3/kavach");
 
 module.exports = async (publicKey) => {
-  try {
-    const address = addressValidator(publicKey);
-    if(!address){
-      throw new Error("Invalid public Key");
-    }
-    const { error, message } = await getAuthMessage(publicKey);
-
-    if (error) {
-      throw new Error(error);
-    }
-    /*
-      return:
-        { data: { message: 'Please sign this message to prove you are owner of this account: 269e5d45-caf7-474d-8167-ab6b140e0249' } }
-    */
-    return { data: { message } };
-  } catch (error) {
-    throw new Error(error);
+  const address = addressValidator(publicKey);
+  if(!address){
+    throw new Error("Invalid public Key");
   }
+  const { error, message } = await getAuthMessage(publicKey);
+
+  if (error) {
+    throw error;
+  }
+  /*
+    return:
+      { data: { message: 'Please sign this message to prove you are owner of this account: 269e5d45-caf7-474d-8167-ab6b140e0249' } }
+  */
+  return { data: { message } };
 };
--- Lighthouse/encryption/shareFile.js
+++ Lighthouse/encryption/shareFile.js
@@ -1,29 +1,25 @@
 const { shareToAddress } = require("@lighthouse-web3/kavach");
 
 module.exports = async (publicKey, shareTo, cid, signedMessage) => {
-  try {
-    const { isSuccess, error } = await shareToAddress(
-      publicKey,
-      cid,
-      signedMessage,
-      shareTo
-    );
+  const { isSuccess, error } = await shareToAddress(
+    publicKey,
+    cid,
+    signedMessage,
+    shareTo
+  );
 
-    if (error) {
-      throw new Error(error);
-    }
-
-    /*
-      {
-        data: {
-          cid: 'QmUHDKv3NNL1mrg4NTW4WwJqetzwZbGNitdjr2G6Z5Xe6s',
-          shareTo: [ '0x487fc2fE07c593EAb555729c3DD6dF85020B5160' ],
-          status: "Success"
-        }
-      }
-    */
-    return { data: { cid, shareTo, status: "Success" } };
-  } catch (error) {
-    throw new Error(error);
+  if (error) {
+    throw error;
   }
+
+  /*
+    {
+      data: {
+        cid: 'QmUHDKv3NNL1mrg4NTW4WwJqetzwZbGNitdjr2G6Z5Xe6s',
+        shareTo: [ '0x487fc2fE07c593EAb555729c3DD6dF85020B5160' ],
+        status: "Success"
+      }
+    }
+  */
+  return { data: { cid, shareTo, status: "Success" } };
 };
