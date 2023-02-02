3d003d06aeaed8c26f948c04d2ed4f6b08ef1235
xlassix <awhy14539@gmail.com>
2023-02-02 01:36:32 +0100

    fix: jest config

---
+++ src/Lighthouse/tests/addCid.test.ts
@@ -0,0 +1,22 @@
+import lighthouse from '..';
+
+describe('Add CID', () => {
+  test('addCid main case', async () => {
+    const response = await lighthouse.addCid(
+      'adiyogi.jpg',
+      'bafkreia4ruswe7ghckleh3lmpujo5asrnd7hrtu5r23zjk2robpcoend34',
+    );
+    expect(response.data).toBe('Added To Queue');
+  }, 20000);
+
+  test('addCid null', async () => {
+    try {
+      const response = await lighthouse.addCid(
+        'adiyogi.jpg',
+        'bafkreia4ruswe7ghckleh3lmpujo5asrnd7hrtu5r23zjk2robpcoend',
+      );
+    } catch (error: any) {
+      expect(typeof error.message).toBe('string');
+    }
+  }, 20000);
+});
---
+++ src/Lighthouse/tests/getUploads.test.ts
@@ -0,0 +1,17 @@
+import lighthouse from '..';
+
+describe('getUpload', () => {
+  test('getUploads', async () => {
+    const response = await lighthouse.getUploads('0x487fc2fE07c593EAb555729c3DD6dF85020B5160');
+
+    expect(typeof response.data.uploads[0]['cid']).toBe('string');
+  }, 20000);
+
+  test('getUploads null case', async () => {
+    try {
+       await lighthouse.getUploads('null');
+    } catch (error: any) {
+      expect(typeof error.message).toBe('string');
+    }
+  }, 20000);
+});
