f513be4034017a553f6917b07f4a1b1e16bc4b65
xlassix <awhy14539@gmail.com>
2023-03-02 01:47:49 +0100

    cli 2.0

---
+++ src/Commands/top-up.ts
@@ -0,0 +1,197 @@
+import chalk from 'chalk';
+import { ethers } from 'ethers';
+import { Spinner } from 'cli-spinner';
+import lighthouse from '../Lighthouse';
+import byteToSize from '../Utils/byteToSize';
+import { getNetwork, config } from '../Utils/getNetwork';
+import Read from 'read';
+
+import { lighthouseConfig } from '../lighthouse.config';
+
+// module.exports = {
+//   command: 'top-up',
+//   desc: 'top up balance',
+//   handler:
+
+export default async function (_amount: number) {
+  try {
+    if (!config.get('LIGHTHOUSE_GLOBAL_PUBLICKEY')) {
+      throw new Error('Wallet not created/imported');
+    }
+    let spinner = new Spinner('');
+    spinner.start();
+
+    const balance = await lighthouse.getBalance(config.get('LIGHTHOUSE_GLOBAL_PUBLICKEY') as string);
+
+    spinner.stop();
+    process.stdout.clearLine(-1);
+    process.stdout.cursorTo(0);
+
+    if (!balance) {
+      throw new Error('Error fetching balance!');
+    }
+
+    // Get key
+    const options = {
+      prompt: 'Enter your password: ',
+      silent: true,
+      default: '',
+    };
+
+    const password: any = await Read(options, (err, res) => console.log(err, res));
+    const decryptedWallet = ethers.Wallet.fromEncryptedJsonSync(
+      config.get('LIGHTHOUSE_GLOBAL_WALLET') as string,
+      password.trim(),
+    );
+
+    if (!decryptedWallet) {
+      throw new Error('Incorrect password!');
+    }
+
+    const network = getNetwork();
+
+    if (network === 'polygon-testnet') {
+      const provider = new ethers.providers.JsonRpcProvider(lighthouseConfig[network]['rpc']);
+
+      const contractABI = [
+        {
+          constant: true,
+          inputs: [{ name: 'account', type: 'address' }],
+          name: 'balanceOf',
+          outputs: [{ name: '', type: 'uint256' }],
+          payable: false,
+          stateMutability: 'view',
+          type: 'function',
+        },
+        {
+          inputs: [],
+          name: 'decimals',
+          outputs: [
+            {
+              internalType: 'uint8',
+              name: '',
+              type: 'uint8',
+            },
+          ],
+          stateMutability: 'view',
+          type: 'function',
+        },
+        {
+          inputs: [
+            {
+              internalType: 'address',
+              name: 'spender',
+              type: 'address',
+            },
+            {
+              internalType: 'uint256',
+              name: 'amount',
+              type: 'uint256',
+            },
+          ],
+          name: 'approve',
+          outputs: [
+            {
+              internalType: 'bool',
+              name: '',
+              type: 'bool',
+            },
+          ],
+          stateMutability: 'nonpayable',
+          type: 'function',
+        },
+      ];
+
+      const contractDepositABI = [
+        {
+          inputs: [
+            {
+              internalType: 'address',
+              name: '_tokenAddress',
+              type: 'address',
+            },
+            {
+              internalType: 'uint256',
+              name: '_amount',
+              type: 'uint256',
+            },
+          ],
+          name: 'addDeposit',
+          outputs: [],
+          stateMutability: 'nonpayable',
+          type: 'function',
+        },
+        {
+          inputs: [
+            {
+              internalType: 'address',
+              name: '_address',
+              type: 'address',
+            },
+          ],
+          name: 'getAvailableSpace',
+          outputs: [
+            {
+              internalType: 'uint256',
+              name: '',
+              type: 'uint256',
+            },
+          ],
+          stateMutability: 'view',
+          type: 'function',
+        },
+      ];
+
+      const contractUSDC = new ethers.Contract(
+        lighthouseConfig[network]['usdc_contract_address'],
+        contractABI,
+        new ethers.Wallet(decryptedWallet.privateKey, provider),
+      );
+      const contractDeposit = new ethers.Contract(
+        lighthouseConfig[network]['deposit_contract_address'],
+        contractDepositABI,
+        new ethers.Wallet(decryptedWallet.privateKey, provider),
+      );
+      console.log(
+        chalk.yellow('USDC Balance: ') +
+          Array(2).fill('\xa0').join('') +
+          ethers.utils.formatUnits(
+            await contractUSDC.balanceOf(config.get('LIGHTHOUSE_GLOBAL_PUBLICKEY')),
+            lighthouseConfig[network]['usd_contract_decimal'],
+          ),
+      );
+      console.log(
+        chalk.yellow('Current contract Storage Balance: ') +
+          (
+            (await contractDeposit.getAvailableSpace(config.get('LIGHTHOUSE_GLOBAL_PUBLICKEY'))) /
+            lighthouseConfig.gbInBytes
+          ).toFixed(2) +
+          'GB',
+      );
+
+      const amount = ethers.utils.parseUnits(String(_amount), await contractUSDC.decimals()).toString();
+
+      spinner = new Spinner(`Getting Approval to spent $${String(_amount)}`);
+      spinner.start();
+
+      let tx = await contractUSDC.approve(lighthouseConfig[network]['deposit_contract_address'], amount);
+      tx = await tx.wait();
+      spinner.stop();
+
+      spinner = new Spinner(`Request top-up for  $${String(_amount)}`);
+      spinner.start();
+      tx = await contractDeposit.addDeposit(lighthouseConfig[network]['usdc_contract_address'], amount, {
+        gasLimit: 500000,
+      });
+
+      tx = await tx.wait();
+      spinner.stop();
+
+      console.log('\n' + chalk.green('successful: ') + lighthouseConfig[network].scan + tx.transactionHash);
+    } else {
+      console.log(chalk.yellow(network) + ': Network not Supported yet for top up');
+    }
+  } catch (error: any) {
+    console.log(chalk.red(error.message));
+  }
+}
---
+++ src/Commands/upload-encrypted.ts
@@ -0,0 +1,175 @@
+import chalk from 'chalk';
+import ethers from 'ethers';
+import { resolve } from 'path';
+import { Spinner } from 'cli-spinner';
+
+import bytesToSize from '../Utils/byteToSize';
+import { getNetwork, config } from '../Utils/getNetwork';
+import { lighthouseConfig } from '../lighthouse.config';
+import lighthouse from '../Lighthouse';
+import Read from 'read';
+
+const getQuote = async (path: string, publicKey: string, Spinner: any) => {
+  const spinner = new Spinner('Getting Quote...');
+  spinner.start();
+
+  const quoteResponse: any = await lighthouse.getQuote(path, publicKey);
+
+  spinner.stop();
+  process.stdout.clearLine(-1);
+  process.stdout.cursorTo(0);
+
+  if (!quoteResponse) {
+    console.log(chalk.red('Error getting quote!'));
+    console.log(chalk.yellow('Check if the wallet is imported!'));
+    process.exit();
+  }
+
+  console.log(
+    chalk.cyan('Name') +
+      Array(30).fill('\xa0').join('') +
+      chalk.cyan('Size') +
+      Array(8).fill('\xa0').join('') +
+      chalk.cyan('Type') +
+      Array(20).fill('\xa0').join(''),
+  );
+
+  for (let i = 0; i < quoteResponse.data.metaData.length; i++) {
+    console.log(
+      quoteResponse.data.metaData[i].fileName +
+        Array(34 - quoteResponse.data.metaData[i].fileName.length)
+          .fill('\xa0')
+          .join('') +
+        bytesToSize(quoteResponse.data.metaData[i].fileSize) +
+        Array(12 - bytesToSize(quoteResponse.data.metaData[i].fileSize).toString().length)
+          .fill('\xa0')
+          .join('') +
+        quoteResponse.data.metaData[i].mimeType,
+    );
+  }
+
+  console.log('\r\n' + chalk.cyan('Summary') + '\r\nTotal Size: ' + bytesToSize(quoteResponse.data.totalSize));
+
+  console.log(
+    'Data Limit: ' +
+      bytesToSize(parseInt(quoteResponse.data.dataLimit)) +
+      '\r\nData Used : ' +
+      bytesToSize(parseInt(quoteResponse.data.dataUsed)) +
+      '\r\nAfter Upload: ' +
+      bytesToSize(
+        parseInt(quoteResponse.data.dataLimit) - (parseInt(quoteResponse.data.dataUsed) + quoteResponse.data.totalSize),
+      ),
+  );
+
+  const remainingAfterUpload =
+    parseInt(quoteResponse.data.dataLimit) - (parseInt(quoteResponse.data.dataUsed) + quoteResponse.data.totalSize);
+
+  return {
+    fileName: quoteResponse.data.metaData[0].fileName,
+    fileSize: quoteResponse.data.metaData[0].fileSize,
+    cost: quoteResponse.data.totalCost,
+    type: quoteResponse.data.type,
+    remainingAfterUpload: remainingAfterUpload,
+  };
+};
+
+const uploadFile = async (path: string, signer: any, apiKey: string, network: any) => {
+  const spinner = new Spinner('Uploading...');
+  spinner.start();
+
+  const messageRequested = (await lighthouse.getAuthMessage(config.get('LIGHTHOUSE_GLOBAL_PUBLICKEY') as string)).data
+    .message;
+  const signedMessage = await signer.signMessage(messageRequested);
+  const uploadResponse = (
+    await lighthouse.uploadEncrypted(path, apiKey, config.get('LIGHTHOUSE_GLOBAL_PUBLICKEY') as string, signedMessage)
+  ).data;
+
+  spinner.stop();
+  process.stdout.clearLine(-1);
+  process.stdout.cursorTo(0);
+
+  if (!uploadResponse.Hash) {
+    console.log(chalk.red('Upload failed!'));
+    console.log(chalk.yellow('Check if api key is correct or create a new key!'));
+    process.exit();
+  }
+
+  console.log(
+    chalk.green('File Uploaded, visit following url to view content!\r\n') +
+      chalk.cyan('Visit: ' + 'https://files.lighthouse.storage/viewFile/' + uploadResponse.Hash),
+  );
+
+  console.log('CID: ' + uploadResponse.Hash);
+  return;
+};
+
+// module.exports = {
+//   command: 'upload-encrypted [path]',
+//   desc: 'Upload a file encrypted',
+//   handler:
+
+export default async function (_path: string) {
+  if (!_path) {
+    console.log(
+      'lighthouse-web3 upload-encrypted <path>\r\n\r\n' +
+        chalk.green('Description: ') +
+        'Upload a file\r\n\r\n' +
+        chalk.cyan('Options:\r\n') +
+        Array(3).fill('\xa0').join('') +
+        '--path: Required, path to file\r\n\r\n' +
+        chalk.magenta('Example:') +
+        Array(3).fill('\xa0').join('') +
+        'lighthouse-web3 upload-encrypted /home/cosmos/Desktop/ILoveAnime.jpg\r\n',
+    );
+  } else {
+    try {
+      // Import nodejs specific library
+      const path = resolve(process.cwd(), _path);
+      const network = getNetwork();
+
+      // Display Quote
+      const quoteResponse = await getQuote(path, config.get('LIGHTHOUSE_GLOBAL_PUBLICKEY') as string, Spinner);
+
+      // Upload
+      console.log(
+        chalk.green('Carefully check the above details are correct, then confirm to complete this upload') + ' Y/n',
+      );
+
+      let options = {
+        prompt: '',
+        silent: false,
+      };
+
+      const selected: any = await Read(options, (err, res) => console.log(err, res));
+
+      if (selected.trim() === 'n' || selected.trim() === 'N' || selected.trim() === 'no') {
+        throw new Error('Canceled Upload');
+      }
+
+      if (!config.get('LIGHTHOUSE_GLOBAL_API_KEY')) {
+        throw new Error('Please create api-key first: use api-key command');
+      }
+
+      if (quoteResponse.remainingAfterUpload < 0) {
+        throw new Error('File size larger than allowed limit. Please Recharge!!!');
+      }
+
+      options = {
+        prompt: 'Enter your password: ',
+        silent: true,
+      };
+      const password: any = await Read(options, (err, res) => console.log(err, res));
+      const decryptedWallet = ethers.Wallet.fromEncryptedJsonSync(
+        config.get('LIGHTHOUSE_GLOBAL_WALLET') as string,
+        password.trim(),
+      );
+
+      const provider = new ethers.providers.JsonRpcProvider(lighthouseConfig[network]['rpc']);
+      const signer = new ethers.Wallet(decryptedWallet.privateKey, provider);
+      const apiKey = config.get('LIGHTHOUSE_GLOBAL_API_KEY') as string;
+      await uploadFile(path, signer, apiKey, network);
+    } catch (error: any) {
+      console.log(chalk.red(error.message));
+    }
+  }
+}
