b7a202ba5a8740be9f897bd5faeb6912767f6b92
ravish <ravish1729@gmail.com>
2023-04-11 00:29:42 +0530

    updated

--- src/Lighthouse/uploadEncrypted/encrypt/text/index.ts
+++ src/Lighthouse/uploadEncrypted/encrypt/text/index.ts
@@ -1,56 +1,16 @@
-/* istanbul ignore file */
-import axios from 'axios'
-import FormData from 'form-data'
-import { encryptFile } from '../../encryptionNode'
-import { generate, saveShards } from '@lighthouse-web3/kavach'
-import { lighthouseConfig } from '../../../../lighthouse.config'
+import browserSide from './browser'
+import serverSide from './node'
 
 export default async (
   text: string,
   apiKey: string,
   publicKey: string,
-  signed_message: string
+  signedMessage: string
 ) => {
-  try {
-    const token = 'Bearer ' + apiKey
-    const endpoint = lighthouseConfig.lighthouseNode + '/api/v0/add'
-
-    // Upload file
-    const formDdata = new FormData()
-
-    const { masterKey: fileEncryptionKey, keyShards } = await generate()
-
-    const encryptedData = await encryptFile(
-      Buffer.from(text),
-      fileEncryptionKey
-    )
-
-    formDdata.append('file', Buffer.from(encryptedData))
-
-    const response = await axios.post(endpoint, formDdata, {
-      withCredentials: true,
-      maxContentLength: Infinity, //this is needed to prevent axios from erroring out with large directories
-      maxBodyLength: Infinity,
-      headers: {
-        'Content-type': `multipart/form-data; boundary= ${formDdata.getBoundary()}`,
-        Encryption: 'true',
-        'Mime-Type': 'text/plain',
-        Authorization: token,
-      },
-    })
-
-    const { error } = await saveShards(
-      publicKey,
-      response.data.Hash,
-      signed_message,
-      keyShards
-    )
-
-    if (error) {
-      throw new Error('Error encrypting file')
-    }
-    return { data: response.data }
-  } catch (error: any) {
-    throw new Error(error.message)
+  // Upload File to IPFS
+  if (typeof process !== 'undefined' && process.release.name === 'node') {
+    return await serverSide(text, apiKey, publicKey, signedMessage)
+  } else {
+    return await browserSide(text, apiKey, publicKey, signedMessage)
   }
 }
