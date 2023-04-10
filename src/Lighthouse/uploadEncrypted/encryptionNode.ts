c34dc8bab837e0c5c830076b47b85b49808b1e20
ravish <ravish1729@gmail.com>
2023-04-10 17:48:02 +0530

    browser fixes

--- src/Lighthouse/uploadEncrypted/encryptionNode.ts
+++ src/Lighthouse/uploadEncrypted/encryptionNode.ts
@@ -1,14 +1,11 @@
-import { Crypto } from '@peculiar/webcrypto'
-
-const crypto = new Crypto()
-
-const importKeyFromBytes = async (keyBytes: any) =>
+const importKeyFromBytes = async (keyBytes: any, crypto: any) =>
   crypto.subtle.importKey('raw', keyBytes, 'PBKDF2', false, ['deriveKey'])
 
 const deriveKey = async (
   sourceKey: any,
   keyUsage: any,
-  keyDerivationParams: any
+  keyDerivationParams: any,
+  crypto: any
 ) =>
   crypto.subtle.deriveKey(
     keyDerivationParams,
@@ -20,20 +17,25 @@ const deriveKey = async (
 
 const encryptFile = async (fileArrayBuffer: any, password: any) => {
   try {
+    const { Crypto } = eval('require')('@peculiar/webcrypto')
+    const crypto = new Crypto()
+
     const plainTextBytes = new Uint8Array(fileArrayBuffer)
     const passwordBytes = new TextEncoder().encode(password)
 
     const salt = crypto.getRandomValues(new Uint8Array(16))
     const iv = crypto.getRandomValues(new Uint8Array(12))
 
-    const passwordKey = await importKeyFromBytes(passwordBytes)
+    const passwordKey = await importKeyFromBytes(passwordBytes, crypto)
 
     const aesKey = await deriveKey(passwordKey, ['encrypt'], {
       name: 'PBKDF2',
       salt: salt,
       iterations: 250000,
       hash: 'SHA-256',
-    })
+    },
+    crypto
+    )
     const cipherBytes = await crypto.subtle.encrypt(
       { name: 'AES-GCM', iv: iv },
       aesKey,
@@ -58,19 +60,24 @@ const encryptFile = async (fileArrayBuffer: any, password: any) => {
 
 const decryptFile = async (cipher: any, password: any) => {
   try {
+    const { Crypto } = eval('require')('@peculiar/webcrypto')
+    const crypto = new Crypto()
+
     const cipherBytes = new Uint8Array(cipher)
     const passwordBytes = new TextEncoder().encode(password)
 
     const salt = cipherBytes.slice(0, 16)
     const iv = cipherBytes.slice(16, 16 + 12)
     const data = cipherBytes.slice(16 + 12)
-    const passwordKey = await importKeyFromBytes(passwordBytes)
+    const passwordKey = await importKeyFromBytes(passwordBytes, crypto)
     const aesKey = await deriveKey(passwordKey, ['decrypt'], {
-      name: 'PBKDF2',
-      salt: salt,
-      iterations: 250000,
-      hash: 'SHA-256',
-    })
+        name: 'PBKDF2',
+        salt: salt,
+        iterations: 250000,
+        hash: 'SHA-256',
+      },
+    crypto
+    )
 
     const decryptedContent = await crypto.subtle.decrypt(
       {
