3a40b23904109d97b9b7716b0f58f09e6c46e38f
ravish <ravish1729@gmail.com>
2023-04-11 14:16:06 +0530

    callback

--- src/Lighthouse/uploadEncrypted/encrypt/file/index.ts
+++ src/Lighthouse/uploadEncrypted/encrypt/file/index.ts
@@ -5,12 +5,19 @@ export default async (
   path: string | any,
   apiKey: string,
   publicKey: string,
-  signedMessage: string
+  signedMessage: string,
+  uploadProgressCallback = (data: any) => {}
 ) => {
   // Upload File to IPFS
   if (typeof process !== 'undefined' && process.release.name === 'node') {
     return await uploadFile(path, apiKey, publicKey, signedMessage)
   } else {
-    return await browser(path, apiKey, publicKey, signedMessage)
+    return await browser(
+      path,
+      apiKey,
+      publicKey,
+      signedMessage,
+      uploadProgressCallback
+    )
   }
 }
