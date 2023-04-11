4f72b783265e7e05af173a268af802448c6727ed
ravish <ravish1729@gmail.com>
2023-04-11 14:13:49 +0530

    cors

--- src/Lighthouse/upload/text/browser.ts
+++ src/Lighthouse/upload/text/browser.ts
@@ -14,7 +14,7 @@ export default async (text: string, apiKey: string) => {
     formDdata.append('file', Buffer.from(text))
 
     const response = await axios.post(endpoint, formDdata, {
-      withCredentials: true,
+      withCredentials: false,
       maxContentLength: Infinity, //this is needed to prevent axios from erroring out with large directories
       maxBodyLength: Infinity,
       headers: {
