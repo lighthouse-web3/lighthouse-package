9d3b93925603f4727a8878d9c5f1f51cba56d70f
ravish <ravish1729@gmail.com>
2023-04-13 03:32:54 +0530

    updated

--- src/Lighthouse/tests/getUploads.test.ts
+++ src/Lighthouse/tests/getUploads.test.ts
@@ -6,7 +6,7 @@ describe('getUpload', () => {
       '0x487fc2fE07c593EAb555729c3DD6dF85020B5160'
     )
 
-    expect(typeof response.data[0]['cid']).toBe('string')
+    expect(typeof response.data.fileList[0]['cid']).toBe('string')
   }, 20000)
 
   test('getUploads null case', async () => {
