bc6b323ffe5b95895356c2012487db5cbccde94b
ravish <ravish1729@gmail.com>
2023-04-14 03:10:19 +0530

    undefined display correct

--- src/Commands/deal-status.ts
+++ src/Commands/deal-status.ts
@@ -1,13 +1,13 @@
-import chalk from 'kleur'
+import { yellow, green, red } from 'kleur'
 import lighthouse from '../Lighthouse'
 import bytesToSize from './utils/byteToSize'
 
 const showResponse = (cid: string, dealStatus: any) => {
   console.log(
-    chalk.yellow('\r\nCID:') +
+    yellow('\r\nCID:') +
       Array(9).fill('\xa0').join('') +
       cid +
-      chalk.yellow('\r\nSize:') +
+      yellow('\r\nSize:') +
       Array(8).fill('\xa0').join('') +
       bytesToSize(dealStatus[0]['content']) +
       '\r\n'
@@ -15,9 +15,9 @@ const showResponse = (cid: string, dealStatus: any) => {
 
   console.log(
     Array(20).fill('\xa0').join('') +
-      chalk.yellow('Miner : ') +
+      yellow('Miner : ') +
       Array(10).fill('\xa0').join('') +
-      chalk.yellow('DealId: ')
+      yellow('DealId: ')
   )
 
   for (let i = 0; i < dealStatus.length; i++) {
@@ -30,7 +30,7 @@ const showResponse = (cid: string, dealStatus: any) => {
     )
   }
   console.log(
-    chalk.green('\r\nView deals at filfox URL:\r\n') +
+    green('\r\nView deals at filfox URL:\r\n') +
       Array(4).fill('\xa0').join('') +
       'https://filfox.info/en/deal/' +
       dealStatus[0]['dealId']
@@ -39,16 +39,16 @@ const showResponse = (cid: string, dealStatus: any) => {
 
 export default async function (data: any, options: any) {
   if (JSON.stringify(data) === '{}') {
-    console.log(chalk.yellow('Select an option:'))
+    console.log(yellow('Select an option:'))
     options.help()
   } else {
     try {
       const dealStatus = (await lighthouse.dealStatus(data)).data
       dealStatus.length === 0
         ? console.log('Deal creation in progress')
-        : showResponse(data.cid, dealStatus)
+        : showResponse(data, dealStatus)
     } catch (error: any) {
-      console.log(chalk.red(error.message))
+      console.log(red(error.message))
     }
   }
 }
