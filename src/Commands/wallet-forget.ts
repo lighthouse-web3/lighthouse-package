8c91face833df725b24d98fbf82f2520ac6107d1
ravish <ravish1729@gmail.com>
2023-04-14 03:08:55 +0530

    kleur lib import style updated

--- src/Commands/wallet-forget.ts
+++ src/Commands/wallet-forget.ts
@@ -1,8 +1,8 @@
-import chalk from 'kleur'
+import { green } from 'kleur'
 import { config } from './utils/getNetwork'
 
 export default async function () {
   config.delete('LIGHTHOUSE_GLOBAL_WALLET')
   config.delete('LIGHTHOUSE_GLOBAL_PUBLICKEY')
-  console.log(chalk.green('Wallet Removed!'))
+  console.log(green('Wallet Removed!'))
 }
--- src/Commands/wallet.ts
+++ src/Commands/wallet.ts
@@ -1,4 +1,4 @@
-import chalk from 'kleur'
+import { red, yellow } from 'kleur'
 import { getNetwork, config } from './utils/getNetwork'
 
 export default async function (data: any) {
@@ -8,15 +8,15 @@ export default async function (data: any) {
     }
 
     console.log(
-      chalk.yellow('Public Key:') +
+      yellow('Public Key:') +
         Array(4).fill('\xa0').join('') +
         config.get('LIGHTHOUSE_GLOBAL_PUBLICKEY')
     )
 
     console.log(
-      chalk.yellow('Network:') + Array(7).fill('\xa0').join('') + getNetwork()
+      yellow('Network:') + Array(7).fill('\xa0').join('') + getNetwork()
     )
   } catch (error: any) {
-    console.log(chalk.red(error.message))
+    console.log(red(error.message))
   }
 }
