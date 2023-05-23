a2590f9432c58c5d75642375623d658d02192738
ravish <ravish1729@gmail.com>
2023-05-24 05:06:15 +0530

    get all ipns records

---
+++ src/Lighthouse/ipns/getAllKeys.ts
@@ -0,0 +1,45 @@
+import axios from 'axios'
+import { lighthouseConfig } from '../../lighthouse.config'
+
+type ipnsObject = {
+  ipnsName: string
+  ipnsId: string
+  publicKey: string
+  cid: string
+  lastUpdate: number
+}
+
+export type keyDataResponse = {
+  data: ipnsObject[]
+}
+
+export default async (apiKey: string): Promise<keyDataResponse> => {
+  try {
+    const response = await axios.get(
+      lighthouseConfig.lighthouseAPI +
+        `/api/ipns/get_ipns_records`,
+      {
+        headers: {
+          Authorization: `Bearer ${apiKey}`,
+        },
+      }
+    )
+    /*
+      return:
+        {
+          data: [
+            {
+              "ipnsName": "6cda213e3a534f8388665dee77a26458",
+              "ipnsId": "k51qzi5uqu5dm6uvby6428rfpcv1vcba6hxq6vcu52qtfsx3np4536jkr71gnu",
+              "publicKey": "0xc88c729ef2c18baf1074ea0df537d61a54a8ce7b",
+              "cid": "Qmd5MBBScDUV3Ly8qahXtZFqyRRfYSmUwEcxpYcV4hzKfW",
+              "lastUpdate": 1684855771773
+            }
+          ]
+        }
+    */
+    return { data: response.data }
+  } catch (error: any) {
+    throw new Error(error.message)
+  }
+}
