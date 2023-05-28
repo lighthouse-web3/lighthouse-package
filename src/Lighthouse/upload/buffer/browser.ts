5db7b5bd483d99ac44775a7046b2d86cfadf6f6f
ravish <ravish1729@gmail.com>
2023-05-28 23:51:48 +0530

    browser support added

---
+++ src/Lighthouse/upload/buffer/browser.ts
@@ -0,0 +1,32 @@
+import axios from 'axios'
+import FormData from 'form-data'
+import { lighthouseConfig } from '../../../lighthouse.config'
+
+export default async (blob: any, apiKey: string, mimeType = '') => {
+  try {
+    const token = 'Bearer ' + apiKey
+    const endpoint = lighthouseConfig.lighthouseNode + '/api/v0/add'
+
+    // Upload file
+    const formData = new FormData()
+
+    formData.append('file', blob)
+    const boundary = Symbol()
+
+    const response = await axios.post(endpoint, formData, {
+      withCredentials: false,
+      maxContentLength: Infinity, //this is needed to prevent axios from erroring out with large directories
+      maxBodyLength: Infinity,
+      headers: {
+        'Content-type': `multipart/form-data; boundary= ${boundary.toString()}`,
+        Encryption: 'false',
+        'Mime-Type': mimeType,
+        Authorization: token,
+      },
+    })
+
+    return { data: response.data }
+  } catch (error: any) {
+    throw new Error(error?.message)
+  }
+}
