7adc2fffc60c3488c148bc3a9dc694c65a61c905
xlassix <awhy14539@gmail.com>
2023-06-14 17:21:54 +0100

    [feat] encrypted MultiUpload

--- src/Lighthouse/uploadEncrypted/encrypt/file/tests/upload.test.ts
+++ src/Lighthouse/uploadEncrypted/encrypt/file/tests/upload.test.ts
@@ -1,5 +1,6 @@
 import uploadFile from '../node'
 import axios from 'axios'
+import { getJWT } from '@lighthouse-web3/kavach'
 import 'dotenv/config'
 import { signAuthMessage } from '../../../../tests/uploadEncrypted.test'
 import { ethers } from 'ethers'
@@ -18,20 +19,30 @@ describe('uploadTest', () => {
     const verificationMessage = (
       await axios.get(
         lighthouseConfig.lighthouseAPI +
-          `/api/auth/get_message?publicKey=${signer.address}`
+          `/api/auth/get_message?publicKey=${signer.address.toLowerCase()}`
       )
     ).data
     const signedMessage = await signer.signMessage(verificationMessage)
-    const apiKey = await lighthouse.getApiKey(signer.address, signedMessage)
+    const apiKey = await lighthouse.getApiKey(
+      signer.address.toLowerCase(),
+      signedMessage
+    )
 
     const signedMessageEncryption = await signAuthMessage(
       '0x8488d2c632da07a93647d7cf701ab6728a884467b1595f3c94007977a20b3539'
     )
+    const { JWT, error } = await getJWT(
+      signer.address.toLowerCase(),
+      signedMessageEncryption
+    )
+    if (error) {
+      throw new Error(error.toString())
+    }
     const data = await uploadFile(
       './src/Lighthouse/uploadEncrypted/encrypt',
       apiKey.data.apiKey,
-      signer.address,
-      signedMessageEncryption
+      signer.address.toLowerCase(),
+      JWT ?? ''
     )
     expect(typeof data).toBe(typeof [])
   })
