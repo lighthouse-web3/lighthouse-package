5e6be0a60414b612862fe92787f503122d2c32f4
ravish <ravish1729@gmail.com>
2023-07-06 08:24:32 +0530

    multiple file param added

--- src/Lighthouse/upload/files/node.ts
+++ src/Lighthouse/upload/files/node.ts
@@ -2,7 +2,6 @@ import axios from 'axios'
 import FormData from 'form-data'
 import basePathConvert from '../../utils/basePathConvert'
 import { lighthouseConfig } from '../../../lighthouse.config'
-import { IFileUploadedResponse } from '../../../types'
 
 export async function walk(dir: string) {
   const { readdir, stat } = eval(`require`)('fs-extra')
@@ -30,18 +29,14 @@ export async function walk(dir: string) {
   @param {string} apiKey - The api key of the user.
 */
 
-export default async (
-  sourcePath: string,
-  apiKey: string
-): Promise<{ data: IFileUploadedResponse[] }> => {
+export default async (sourcePath: string, apiKey: string, multi: boolean) => {
   const { createReadStream, lstatSync } = eval(`require`)('fs-extra')
   const mime = eval(`require`)('mime-types')
 
   const token = 'Bearer ' + apiKey
   const stats = lstatSync(sourcePath)
   try {
-    const endpoint =
-      lighthouseConfig.lighthouseNode + '/api/v0/add?wrap-with-directory=false'
+    const endpoint = lighthouseConfig.lighthouseNode + `/api/v0/add?wrap-with-directory=${multi}`
     if (stats.isFile()) {
       //we need to create a single read stream instead of reading the directory recursively
       const data = new FormData()
@@ -61,7 +56,7 @@ export default async (
         },
       })
 
-      return { data: [response.data] }
+      return { data: response.data }
     } else {
       const files = await walk(sourcePath)
       const data = new FormData()
@@ -86,14 +81,24 @@ export default async (
         },
       })
 
-      if (typeof response.data === 'string') {
+      if (typeof response.data === 'string' && multi) {
         response.data = JSON.parse(
           `[${response.data.slice(0, -1)}]`.split('\n').join(',')
         )
       } else {
-        response.data = [response.data]
+        const temp = response.data.split('\n')
+        response.data = JSON.parse(temp[temp.length - 2])
       }
 
+      /*
+        {
+          data: {
+            Name: 'flow1.png',
+            Hash: 'QmUHDKv3NNL1mrg4NTW4WwJqetzwZbGNitdjr2G6Z5Xe6s',
+            Size: '31735' 
+          }
+        }
+      */
       return { data: response.data }
     }
   } catch (error: any) {
