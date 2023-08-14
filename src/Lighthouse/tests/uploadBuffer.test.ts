8ab027f13ce57ac1b5822b4d1c5f8a3acca153a3
Parva Jain <parv@Parvas-MacBook-Air.local>
2023-08-14 18:17:58 +0530

    uploading to ipfs tests

--- src/Lighthouse/tests/uploadBuffer.test.ts
+++ src/Lighthouse/tests/uploadBuffer.test.ts
@@ -1,27 +1,15 @@
-import axios from 'axios'
-import { ethers } from 'ethers'
 import lighthouse from '..'
-import { lighthouseConfig } from '../../lighthouse.config'
-describe('uploadBuffer', () => {
-  test('upload Buffer Main Case File', async () => {
-    const image =
-      'iVBORw0KGgoAAAANSUhEUgAAAA8AAAAMCAYAAAC9QufkAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAADCSURBVChTrZJRCoMwDIaTtM56hsEeRNjDELz/yeas7ZLYMBWEOfZBiH+axKSK7bXNcJKcpSQDpZTgrEmx1NPS6zf+VIwsCAHZ9khczJAcjRUNiAjOu02SQY70zJAc0djdukxYenCdXEaak0rnSoH1s+9SNPb3PnvvYY4zxBiXaKEOtfrxOaqvLhW/lWB6TXrrJOMq7OTZTJKMdXyjh8eQ2em4dvgtvPqn2yEH/yD51S0KMm7TBKjrZV9l1/fCu4cmwBsXPlBp+IIIrQAAAABJRU5ErkJggg=='
-    const publicKey = '0x5129B1153f4f9f321F41CbA831899336CB4134c7' // wallet 7
-    const verificationMessage = (
-      await axios.get(
-        lighthouseConfig.lighthouseAPI +
-          `/api/auth/get_message?publicKey=${publicKey}`
-      )
-    ).data
-    const signer = new ethers.Wallet(
-      '0xace7b85099ef79e3ddea94bd44f6c42fe75e447388af464cc7d4467965f09322'
-    )
-    const signedMessage = await signer.signMessage(verificationMessage)
-    const apiKey = await lighthouse.getApiKey(publicKey, signedMessage)
+import 'dotenv/config'
 
-    const deployResponse = (
-      await lighthouse.uploadBuffer(image, apiKey.data.apiKey)
-    ).data
+describe('uploadBuffer', () => {
+  const apiKey = process.env.TEST_API_KEY
+  const image =
+    'iVBORw0KGgoAAAANSUhEUgAAAA8AAAAMCAYAAAC9QufkAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAADCSURBVChTrZJRCoMwDIaTtM56hsEeRNjDELz/yeas7ZLYMBWEOfZBiH+axKSK7bXNcJKcpSQDpZTgrEmx1NPS6zf+VIwsCAHZ9khczJAcjRUNiAjOu02SQY70zJAc0djdukxYenCdXEaak0rnSoH1s+9SNPb3PnvvYY4zxBiXaKEOtfrxOaqvLhW/lWB6TXrrJOMq7OTZTJKMdXyjh8eQ2em4dvgtvPqn2yEH/yD51S0KMm7TBKjrZV9l1/fCu4cmwBsXPlBp+IIIrQAAAABJRU5ErkJggg=='
+
+  it('should upload buffer to ipfs when valid API key provided', async () => {
+    const deployResponse = (await lighthouse.uploadBuffer(image, apiKey)).data
+
+    expect(deployResponse['Name']).toEqual(deployResponse['Hash'])
 
     expect(deployResponse).toHaveProperty('Name')
     expect(typeof deployResponse['Name']).toBe('string')
@@ -31,5 +19,15 @@ describe('uploadBuffer', () => {
 
     expect(deployResponse).toHaveProperty('Size')
     expect(typeof deployResponse['Size']).toBe('string')
+  }, 60000)
+
+  it('should not upload buffer to ipfs when invalid API key provided', async () => {
+    try {
+      const deployResponse = (
+        await lighthouse.uploadBuffer(image, 'invalid.apiKey')
+      ).data
+    } catch (error) {
+      expect(error.message).toBe('Request failed with status code 500')
+    }
   }, 60000)
 })
