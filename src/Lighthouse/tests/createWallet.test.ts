2d529b011594f3a9e14a3a89e22ef5e7b3f41c27
Parva Jain <parv@Parvas-MacBook-Air.local>
2023-08-14 18:16:02 +0530

    wallet creation tests

--- src/Lighthouse/tests/createWallet.test.ts
+++ src/Lighthouse/tests/createWallet.test.ts
@@ -1,19 +1,49 @@
 import lighthouse from '..'
+import { ethers } from 'ethers'
 
-describe('create Wallet', () => {
-  test('createWallet', async () => {
-    const wallet = (await lighthouse.createWallet('Uchihas')).data
-      .encryptedWallet
-    console.log(wallet)
+describe('createWallet', () => {
+  let password = 'Uchiha'
+  // let password = 100001
+  let wallet
+
+  // this test case should execute the catch block
+  it('should not create a new wallet using non-string password', async () => {
+    try {
+      wallet = (await lighthouse.createWallet(100001)).data.encryptedWallet
+      console.log('wallet created ')
+    } catch (error) {
+      console.log(error)
+      expect(typeof error.message).toBe('string')
+    }
+  }, 20000)
+
+  it('should create a new wallet using string password', async () => {
+    wallet = (await lighthouse.createWallet(password)).data.encryptedWallet
+    // console.log(wallet)
     const walletParse = JSON.parse(wallet)
     expect(walletParse).toHaveProperty('address')
   }, 20000)
 
-  test('createWallet null', async () => {
+  it('should not allow decripting wallet using incorrect password', async () => {
     try {
-      const wallet = await lighthouse.createWallet('null')
-    } catch (error: any) {
-      expect(typeof error.message).toBe('string')
+      const decryptedWallet = ethers.Wallet.fromEncryptedJsonSync(
+        wallet,
+        'randomPassword'
+      )
+    } catch (error) {
+      // console.log(error.message)
+      expect(error.message).toEqual('invalid password')
     }
+  }, 20000)
+
+  it('should allow decripting wallet using correct password', async () => {
+    const decryptedWallet = ethers.Wallet.fromEncryptedJsonSync(
+      wallet,
+      password
+    )
+    const publicAddress = '0x' + JSON.parse(wallet).address
+    console.log(decryptedWallet.address)
+    console.log(decryptedWallet.privateKey)
+    expect(decryptedWallet.address.toLowerCase()).toEqual(publicAddress)
   }, 20000)
 })
