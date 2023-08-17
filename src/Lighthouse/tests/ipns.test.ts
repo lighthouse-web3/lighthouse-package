22ee7ff4f62e9f945f49b9203004f49582374eaa
Parva Jain <parv@Parvas-MacBook-Air.local>
2023-08-17 22:07:43 +0530

    ipns tests added

--- src/Lighthouse/tests/ipns.test.ts
+++ src/Lighthouse/tests/ipns.test.ts
@@ -1,46 +1,73 @@
-import {ethers} from 'ethers'
+import { ethers } from 'ethers'
 import axios from 'axios'
 import lighthouse from '../../Lighthouse'
 import { lighthouseConfig } from '../../lighthouse.config'
+import 'dotenv/config'
 
-test('IPNS', async () => {
-  const publicKey = '0xEaF4E24ffC1A2f53c07839a74966A6611b8Cb8A1'
-  const verificationMessage = (
-    await axios.get(
-      lighthouseConfig.lighthouseAPI +
-        `/api/auth/get_message?publicKey=${publicKey}`
-    )
-  ).data
-  const provider = ethers.getDefaultProvider()
-  const signer = new ethers.Wallet(
-    '0x8218aa5dbf4dbec243142286b93e26af521b3e91219583595a06a7765abc9c8b',
-    provider
-  )
-  const signedMessage = await signer.signMessage(verificationMessage)
-  const response = await lighthouse.getApiKey(publicKey, signedMessage)
-  const apiKey = response.data.apiKey
+describe('ipns', () => {
+  const apiKey = process.env.TEST_API_KEY
+  let ipnsName
+  describe('generateKey', () => {
+    it('should generate Key using valid apiKey', async () => {
+      const key = (await lighthouse.generateKey(apiKey)).data
+      expect(key).toHaveProperty('ipnsName')
+      expect(key).toHaveProperty('ipnsId')
+      ipnsName = key.ipnsName
+    }, 20000)
 
-  //Generate key
-  const key = (
-    await lighthouse.generateKey(apiKey)
-  ).data
+    it('should not generate Key using invalid apiKey', async () => {
+      try {
+        const key = (await lighthouse.generateKey('invalidApiKey')).data
+      } catch (error) {
+        expect(error.message).toBe('Request failed with status code 401')
+      }
+    }, 20000)
+  })
 
-  expect(typeof key.ipnsName).toBe('string')
-  expect(typeof key.ipnsId).toBe('string')
-  
-  //List keys
-  const keyList = (await lighthouse.getAllKeys(apiKey)).data
-  expect(typeof keyList.length).toBe('number')
-  
-  //Publish record
-  const publishRes = (await lighthouse.publishRecord(
-    'Qmd5MBBScDUV3Ly8qahXtZFqyRRfYSmUwEcxpYcV4hzKfW',
-    key.ipnsName,
-    apiKey
-  )).data
-  expect(typeof publishRes.Name).toBe('string')
-  
-  //Remove Key
-  const removeRes = await lighthouse.removeKey(key.ipnsName, apiKey)
-  expect(typeof removeRes.data.Keys.length).toBe('number')
-}, 40000)
+  describe('publishRecord', () => {
+    const cid = 'Qmd5MBBScDUV3Ly8qahXtZFqyRRfYSmUwEcxpYcV4hzKfW'
+    it('should publish data using correct ipnsName', async () => {
+      const publishRes = (await lighthouse.publishRecord(cid, ipnsName, apiKey))
+        .data
+      expect(publishRes).toHaveProperty('Name')
+      expect(publishRes).toHaveProperty('Value')
+      expect(publishRes.Value).toEqual(`/ipfs/${cid}`)
+    }, 20000)
+
+    it('should not publish data using incorrect ipnsName', async () => {
+      try {
+        const publishRes = (
+          await lighthouse.publishRecord(cid, 'invalid ipnsName', apiKey)
+        ).data
+      } catch (error) {
+        expect(error.message).toBe('Request failed with status code 400')
+      }
+    }, 20000)
+  })
+
+  describe('getAllKeys', () => {
+    it('should retrieve list of all keys from apiKey', async () => {
+      const keyList = (await lighthouse.getAllKeys(apiKey)).data
+      const len = keyList.length
+      expect(keyList[len - 1].ipnsName).toEqual(ipnsName)
+    }, 20000)
+  })
+
+  describe('removeKey', () => {
+    it('should remove key from key list', async () => {
+      const lenBefore = (await lighthouse.getAllKeys(apiKey)).data.length
+      const response = (await lighthouse.removeKey(ipnsName, apiKey)).data
+      const lenAfter = (await lighthouse.getAllKeys(apiKey)).data.length
+      expect(response.Keys[0].Name).toBe(ipnsName)
+      expect(lenBefore - lenAfter).toEqual(1)
+    }, 20000)
+
+    it('should throw error when removing non existent/already removed key', async () => {
+      try {
+        const response = (await lighthouse.removeKey(ipnsName, apiKey)).data
+      } catch (error) {
+        expect(error.message).toBe('Request failed with status code 400')
+      }
+    })
+  })
+})
