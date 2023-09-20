73ffaea8459396428bd67ba10a0bd0dfcff75e7d
ravish <ravish1729@gmail.com>
2023-09-20 20:57:34 +0530

    type

--- src/Lighthouse/tests/getApiKey.spec.ts
+++ src/Lighthouse/tests/getApiKey.spec.ts
@@ -5,15 +5,14 @@ import lighthouse from '..'
 import 'dotenv/config'
 
 describe('getApiKey', () => {
-  let pubKey = process.env.TEST_PUBLIC_KEY
-  console.log(pubKey)
-  let privKey = process.env.TEST_PRIVATE_KEY
-  let verificationMessage
+  let publicKey = process.env.TEST_PUBLIC_KEY as string
+  let privateKey = process.env.TEST_PRIVATE_KEY as string
+  let verificationMessage: any
 
   it('should get verification message from server using public key', async () => {
     verificationMessage = await axios.get(
       lighthouseConfig.lighthouseAPI +
-        `/api/auth/get_message?publicKey=${pubKey}`
+        `/api/auth/get_message?publicKey=${publicKey}`
     )
 
     expect(verificationMessage.status).toEqual(200)
@@ -22,13 +21,13 @@ describe('getApiKey', () => {
 
   it('should not allow random private key to sign message and get API Key', async () => {
     try {
-      const provider = ethers.getDefaultProvider()
+      const provider = ethers.getDefaultProvider('homestead')
       const signer = new ethers.Wallet(
         '0x8218aa5dbf4dbec243142286b93e26af521b3e91219583595a06a7765abc9c8b',
         provider
       )
       const signedMessage = await signer.signMessage(verificationMessage.data)
-      const response = await lighthouse.getApiKey(pubKey, signedMessage)
+      const response = await lighthouse.getApiKey(publicKey, signedMessage)
     } catch (error) {
       expect(typeof error.message).toBe('string')
     }
@@ -36,13 +35,13 @@ describe('getApiKey', () => {
 
   it('should not generate api Key with random signed message', async () => {
     try {
-      const provider = ethers.getDefaultProvider()
+      const provider = ethers.getDefaultProvider('homestead')
       const signer = new ethers.Wallet(
         '0x8218aa5dbf4dbec243142286b93e26af521b3e91219583595a06a7765abc9c8b',
         provider
       )
       const signedMessage = await signer.signMessage('randomMessage')
-      const response = await lighthouse.getApiKey(pubKey, signedMessage)
+      const response = await lighthouse.getApiKey(publicKey, signedMessage)
     } catch (error) {
       console.log(error.message)
       expect(typeof error.message).toBe('string')
@@ -51,10 +50,10 @@ describe('getApiKey', () => {
 
   it('should only allow corresponding private key to sign verification message and get API Key', async () => {
     try {
-      const provider = ethers.getDefaultProvider()
-      const signer = new ethers.Wallet(privKey, provider)
+      const provider = ethers.getDefaultProvider('homestead')
+      const signer = new ethers.Wallet(privateKey, provider)
       const signedMessage = await signer.signMessage(verificationMessage.data)
-      const response = await lighthouse.getApiKey(pubKey, signedMessage)
+      const response = await lighthouse.getApiKey(publicKey, signedMessage)
       expect(response.data).toHaveProperty('apiKey')
       const apiKey = response.data.apiKey
     } catch (error) {
