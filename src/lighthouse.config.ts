5bc604df6ed0d29b7f71c69b4ed06084b97f16a7
ravish <ravish1729@gmail.com>
2023-11-22 14:45:26 +0530

    calibration added

--- src/lighthouse.config.ts
+++ src/lighthouse.config.ts
@@ -3,7 +3,6 @@ const defaultConfig = {
   lighthouseNode: 'https://node.lighthouse.storage',
   lighthouseGateway: 'https://gateway.lighthouse.storage',
   lighthouseBLSNode: 'https://encryption.lighthouse.storage',
-  lighthouseDataDepot: 'https://data-depot.lighthouse.storage',
   network: 'polygon',
   fantom: {
     symbol: 'FTM',
@@ -26,8 +25,10 @@ const defaultConfig = {
     usdt_contract_address: '0xc2132D05D31c914a87C6611C10748AEb04B58e8F',
     usdc_contract_address: '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174',
     dai_contract_address: '0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063',
-    usd_contract_decimal: 6,
+    usdt_contract_decimal: 6,
+    usdc_contract_decimal: 6,
     dai_contract_decimal: 18,
+    native_decimal: 18
   },
   binance: {
     symbol: 'BNB',
@@ -52,6 +53,16 @@ const defaultConfig = {
     dai_contract_address: '0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1',
     usd_contract_decimal: 6,
     dai_contract_decimal: 18,
+  },
+  'calibration': {
+    symbol: 'FIL',
+    rpc: 'https://api.calibration.node.glif.io/rpc/v1',
+    scan: 'https://calibration.filfox.info/en/message/',
+    chain_id: '314159',
+    lighthouse_contract_address: '0x808a85bbFD82369A3B0c6eFFd4Be1d475F05a6a0',
+    usdt_contract_address: '0xA471B7936906fF810865e52AF86C12B9865C850A',
+    usdt_contract_decimal: 18,
+    native_decimal: 18
   },
   'fantom-testnet': {
     symbol: 'FTM',
