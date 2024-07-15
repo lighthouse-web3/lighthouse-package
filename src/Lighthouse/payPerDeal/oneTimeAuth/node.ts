c3cfa80413f05174f1c8aa4cfdb98565d5f2b587
ravish1729 <ravish1729@gmail.com>
2024-07-16 02:01:34 +0530

    axios removed

--- src/Lighthouse/payPerDeal/oneTimeAuth/node.ts
+++ src/Lighthouse/payPerDeal/oneTimeAuth/node.ts
@@ -1,20 +1,21 @@
 /* istanbul ignore file */
-import axios from 'axios'
 import { ethers } from 'ethers'
 import { lighthouseConfig } from '../../../lighthouse.config'
 
-export default async (privateKey: string|undefined): Promise<string> => {
-	try {
-		if(!privateKey) {
-			throw new Error("Private key not provided!!!")
-		}
-		const signer = new ethers.Wallet(privateKey)
-		const message = (await axios.get(
-			`${lighthouseConfig.lighthouseAPI}/api/auth/get_message?publicKey=${signer.address}`
-		)).data
-		const signature = await signer.signMessage(message)
-		return `${signer.address}$${signature}`
-	} catch (error: any) {
-		throw new Error(error.message)
-	}
+export default async (privateKey: string | undefined): Promise<string> => {
+  try {
+    if (!privateKey) {
+      throw new Error('Private key not provided!!!')
+    }
+    const signer = new ethers.Wallet(privateKey)
+    // Replace axios call with fetch
+    const response = await fetch(
+      `${lighthouseConfig.lighthouseAPI}/api/auth/get_message?publicKey=${signer.address}`
+    )
+    const message = (await response.json()) as string
+    const signature = await signer.signMessage(message)
+    return `${signer.address}$${signature}`
+  } catch (error: any) {
+    throw new Error(error.message)
+  }
 }
--- src/Lighthouse/payPerDeal/oneTimeAuth/web.ts
+++ src/Lighthouse/payPerDeal/oneTimeAuth/web.ts
@@ -1,19 +1,19 @@
 /* istanbul ignore file */
-import axios from 'axios'
 import { ethers } from 'ethers'
 import { lighthouseConfig } from '../../../lighthouse.config'
 
 export default async (): Promise<string> => {
-	try {
-		//@ts-ignore
-		const provider = new ethers.BrowserProvider((window as any).ethereum)
-    	const signer = await provider.getSigner()
-		const message = (await axios.get(
-			`${lighthouseConfig.lighthouseAPI}/api/auth/get_message?publicKey=${signer.address}`
-		)).data
-		const signature = await signer.signMessage(message)
-		return `${signer.address}$${signature}`
-	} catch (error: any) {
-		throw new Error(error.message)
-	}
+  try {
+    //@ts-ignore
+    const provider = new ethers.BrowserProvider((window as any).ethereum)
+    const signer = await provider.getSigner()
+    const response = await fetch(
+      `${lighthouseConfig.lighthouseAPI}/api/auth/get_message?publicKey=${signer.address}`
+    )
+    const message = (await response.json()) as string
+    const signature = await signer.signMessage(message)
+    return `${signer.address}$${signature}`
+  } catch (error: any) {
+    throw new Error(error.message)
+  }
 }
