a32d98225b8c0c68a6fbb557611381c39f91caaa
ravish1729 <ravish1729@gmail.com>
2024-07-16 03:14:12 +0530

    axios removed

--- src/Lighthouse/uploadEncrypted/decrypt/node.ts
+++ src/Lighthouse/uploadEncrypted/decrypt/node.ts
@@ -1,22 +1,24 @@
 /* istanbul ignore file */
-import axios from 'axios'
 import { decryptFile } from '../encryptionNode'
 import { lighthouseConfig } from '../../../lighthouse.config'
 
 export default async (cid: string, fileEncryptionKey: any) => {
   try {
-    const result = await axios.post(
+    const response = await fetch(
       lighthouseConfig.lighthouseGateway + '/api/v0/cat/' + cid,
-      null,
       {
-        responseType: 'arraybuffer',
+        method: 'POST',
       }
     )
 
-    const decrypted = await decryptFile(
-      Buffer.from(result.data),
-      fileEncryptionKey
-    )
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
+
+    const arrayBuffer = await response.arrayBuffer()
+    const result = Buffer.from(arrayBuffer)
+
+    const decrypted = await decryptFile(result, fileEncryptionKey)
     return decrypted
   } catch (error: any) {
     throw new Error(error.message)
