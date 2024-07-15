a7dc8da69006a73cb09f88929bc9c9c80bb1f63a
ravish1729 <ravish1729@gmail.com>
2024-07-16 01:58:48 +0530

    lint

--- src/Lighthouse/dealStatus/index.ts
+++ src/Lighthouse/dealStatus/index.ts
@@ -29,13 +29,13 @@ export default async (cid: string): Promise<dealResponse> => {
   try {
     const response = await fetch(
       `${lighthouseConfig.lighthouseAPI}/api/lighthouse/deal_status?cid=${cid}`
-    );
+    )
     if (!response.ok) {
-      throw new Error(`Request failed with status code ${response.status}`);
+      throw new Error(`Request failed with status code ${response.status}`)
     }
-    const dealStatus: dealData[] = await response.json() as dealData[];
-    
-    return { data: dealStatus };
+    const dealStatus: dealData[] = (await response.json()) as dealData[]
+
+    return { data: dealStatus }
   } catch (error: any) {
     throw new Error(error.message)
   }
