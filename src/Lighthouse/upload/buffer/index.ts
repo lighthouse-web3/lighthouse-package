d51308823e1c991a8fd33b1a649dcb9829c8a01d
ravish1729 <ravish1729@gmail.com>
2024-07-16 02:02:33 +0530

    lint

--- src/Lighthouse/upload/buffer/index.ts
+++ src/Lighthouse/upload/buffer/index.ts
@@ -1,13 +1,10 @@
 import uploadBuffer from './node'
 import uploadTypedArray from './browser'
 
-export default async (
-  buffer: any, 
-  apiKey: string
-) => {
+export default async (buffer: any, apiKey: string) => {
   // Upload File to IPFS
   //@ts-ignore
-  if (typeof window === "undefined") {
+  if (typeof window === 'undefined') {
     return await uploadBuffer(buffer, apiKey)
   } else {
     return await uploadTypedArray(buffer, apiKey)
