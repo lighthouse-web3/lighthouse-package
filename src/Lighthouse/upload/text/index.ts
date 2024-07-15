1825bfa29208974db8a45323a6afd7b345c2189f
ravish1729 <ravish1729@gmail.com>
2024-07-16 02:03:41 +0530

    lint

--- src/Lighthouse/upload/text/index.ts
+++ src/Lighthouse/upload/text/index.ts
@@ -1,10 +1,10 @@
 import uploadTextServer from './node'
 import uploadTextBrowser from './browser'
 
-export default async (text: string, apiKey: string, name='text') => {
+export default async (text: string, apiKey: string, name = 'text') => {
   // Upload File to IPFS
   //@ts-ignore
-  if (typeof window === "undefined") {
+  if (typeof window === 'undefined') {
     return await uploadTextServer(text, apiKey, name)
   } else {
     return await uploadTextBrowser(text, apiKey, name)
