b582cd96f47c6d8296080dd0d074f30171f046be
ravish1729 <ravish1729@gmail.com>
2025-05-30 14:25:11 +0530

    version

--- src/Commands/index.ts
+++ src/Commands/index.ts
@@ -72,7 +72,7 @@ Command.prototype.helpInformation = function (context: any) {
 }
 
 widgets.addHelpText('before', 'Welcome to lighthouse-web3')
-widgets.version('0.3.7')
+widgets.version('0.3.9')
 
 widgets
   .command('wallet')
