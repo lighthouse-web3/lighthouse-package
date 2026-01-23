fe540b5d1cbbc79f591491cb8c454fdb617da799
ravish1729 <ravish1729@gmail.com>
2026-01-23 19:17:30 +0530

    storage type added

--- src/Lighthouse/upload/files/index.ts
+++ src/Lighthouse/upload/files/index.ts
@@ -2,26 +2,33 @@ import uploadFile from './node'
 import uploadFileBrowser from './browser'
 import {
   IUploadProgressCallback,
-  IFileUploadedResponse
+  IFileUploadedResponse,
+  UploadFilesOptions
 } from '../../../types'
 
 async function uploadFiles(
   path: string | any,
   apiKey: string,
-  cidVersion: number = 1,
+  cidVersionOrOptions: number | UploadFilesOptions = 1,
   uploadProgressCallback?: (data: IUploadProgressCallback) => void
 ): Promise<{ data: IFileUploadedResponse }> {
+  // Normalize to options object (hybrid support)
+  const options: UploadFilesOptions =
+    typeof cidVersionOrOptions === 'number'
+      ? {
+          cidVersion: cidVersionOrOptions,
+          onProgress: uploadProgressCallback,
+        }
+      : cidVersionOrOptions
+
+  const { cidVersion = 1, onProgress, headers } = options
+
   // Upload File to IPFS
   //@ts-ignore
   if (typeof window === 'undefined') {
-    return await uploadFile(path, apiKey, cidVersion, uploadProgressCallback)
+    return await uploadFile(path, apiKey, cidVersion, headers, onProgress)
   } else {
-    return await uploadFileBrowser(
-      path,
-      apiKey,
-      cidVersion,
-      uploadProgressCallback
-    )
+    return await uploadFileBrowser(path, apiKey, cidVersion, headers, onProgress)
   }
 }
 
