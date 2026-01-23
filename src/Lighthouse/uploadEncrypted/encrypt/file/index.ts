02e89f5fafb866ce272d6cd51c377ee1e510f193
ravish1729 <ravish1729@gmail.com>
2026-01-23 19:27:49 +0530

    storage type added

--- src/Lighthouse/uploadEncrypted/encrypt/file/index.ts
+++ src/Lighthouse/uploadEncrypted/encrypt/file/index.ts
@@ -1,18 +1,43 @@
 import browser from './browser'
 import uploadFile from './node'
+import { UploadFilesOptions } from '../../../../types'
 
 export default async (
   path: string | any,
   apiKey: string,
   publicKey: string,
   signedMessage: string,
-  cidVersion: number = 1,
+  cidVersionOrOptions: number | UploadFilesOptions = 1,
   uploadProgressCallback?: (data: any) => void
 ) => {
+  // Normalize to options object (hybrid support)
+  const options: UploadFilesOptions =
+    typeof cidVersionOrOptions === 'number'
+      ? {
+          cidVersion: cidVersionOrOptions,
+          onProgress: uploadProgressCallback,
+        }
+      : cidVersionOrOptions
+
+  const { cidVersion = 1, onProgress, headers } = options
+  const progressCallback =
+    onProgress ||
+    uploadProgressCallback ||
+    (() => {
+      return
+    })
+
   // Upload File to IPFS
   //@ts-ignore
   if (typeof window === 'undefined') {
-    return await uploadFile(path, apiKey, publicKey, signedMessage, cidVersion)
+    return await uploadFile(
+      path,
+      apiKey,
+      publicKey,
+      signedMessage,
+      cidVersion,
+      headers
+    )
   } else {
     return await browser(
       path,
@@ -20,10 +45,8 @@ export default async (
       publicKey,
       signedMessage,
       cidVersion,
-      uploadProgressCallback ||
-        (() => {
-          return
-        })
+      headers,
+      progressCallback,
     )
   }
 }
--- src/Lighthouse/uploadEncrypted/encrypt/text/index.ts
+++ src/Lighthouse/uploadEncrypted/encrypt/text/index.ts
@@ -1,17 +1,52 @@
 import serverSide from './node'
 import browser from './browser'
+import { UploadOptions, Headers } from '../../../../types'
+
+type EncryptedTextUpload = (
+  text: string,
+  apiKey: string,
+  publicKey: string,
+  signedMessage: string,
+  name: string,
+  cidVersion: number,
+  headers?: Headers,
+) => Promise<any>
 
 export default async (
   text: string,
   apiKey: string,
   publicKey: string,
   signedMessage: string,
-  name = 'text'
+  name = 'text',
+  options: UploadOptions = {
+    cidVersion: 1,
+    headers: {},
+  }
 ) => {
+  const { cidVersion = 1, headers } = options
+  const serverSideUpload = serverSide as EncryptedTextUpload
+  const browserUpload = browser as EncryptedTextUpload
+
   //@ts-ignore
   if (typeof window === 'undefined') {
-    return await serverSide(text, apiKey, publicKey, signedMessage, name)
+    return await serverSideUpload(
+      text,
+      apiKey,
+      publicKey,
+      signedMessage,
+      name,
+      cidVersion,
+      headers
+    )
   } else {
-    return await browser(text, apiKey, publicKey, signedMessage, name)
+    return await browserUpload(
+      text,
+      apiKey,
+      publicKey,
+      signedMessage,
+      name,
+      cidVersion,
+      headers
+    )
   }
 }
