66032773d1e0c08db570dfc44ffc3438556670cc
ravish1729 <ravish1729@gmail.com>
2026-01-23 19:50:27 +0530

    types update

--- src/types.ts
+++ src/types.ts
@@ -11,3 +11,16 @@ export interface IFileUploadedResponse {
 export type UploadFileReturnType<T extends boolean> = T extends true
   ? IFileUploadedResponse[]
   : IFileUploadedResponse
+
+export type Headers = {
+  storageType?: string
+}
+export interface UploadFilesOptions {
+  cidVersion?: number
+  onProgress?: (data: IUploadProgressCallback) => void
+  headers?: Headers
+}
+export interface UploadOptions {
+  cidVersion?: number
+  headers?: Headers
+}
